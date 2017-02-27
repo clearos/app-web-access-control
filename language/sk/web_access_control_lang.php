53eecac6e5959624084819ee9705b54ce5a2abe2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:23:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Pridať časovej periódy';
+$lang['web_access_control_app_description'] = 'Control Web Access umožňuje správcovi vynútiť time-of-dňový webový prístup ku skupinám alebo počítačov (IP alebo MAC adresy) za použitia web proxy.';
+$lang['web_access_control_app_name'] = 'Control Web Access';
+$lang['web_access_control_apply_group'] = 'Použiť riadenie prístupu do skupiny';
+$lang['web_access_control_apply_ip'] = 'Použiť riadenie prístupu na IP adresy';
+$lang['web_access_control_apply_mac'] = 'Použiť riadenie prístupu MAC Address';
+$lang['web_access_control_ctrl_click'] = '(CTRL-kliknite pre viac výberov)';
+$lang['web_access_control_day_of_week'] = 'Deň v týždni';
+$lang['web_access_control_end_time'] = 'end Time';
+$lang['web_access_control_id_method'] = 'spôsob identifikácie';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'názov';
+$lang['web_access_control_outside'] = 'zvonka';
+$lang['web_access_control_outside_range'] = 'Mimo časové obmedzenia';
+$lang['web_access_control_priority'] = 'priorita';
+$lang['web_access_control_start_time'] = 'Doba spustenia';
+$lang['web_access_control_time_definitions'] = 'Dennej doby Definícia';
+$lang['web_access_control_time_delete_warning'] = 'Upozornenie: všetky položky riadenie prístupu používajúce túto definíciu času budú vymazané.';
+$lang['web_access_control_time_of_day'] = 'Čas dňa';
+$lang['web_access_control_time_restriction'] = 'obmedzenia';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'typ';
+$lang['web_access_control_web_access_control'] = 'Control Web Access';
+$lang['web_access_control_within'] = 'vnútri';
+$lang['web_access_control_within_range'] = 'V rámci časových obmedzení';
