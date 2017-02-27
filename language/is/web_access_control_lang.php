a2a82f0e241497f008e53a37bffa112bb050e9be
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:18:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Bæta tímabili';
+$lang['web_access_control_app_description'] = 'Web Access Control gerir stjórnandi til að framfylgja tíma á dag vefaðgang að hópa eða tölvur (IP eða MAC heimilisfang) með Web Proxy.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Berið Access Control í hóp';
+$lang['web_access_control_apply_ip'] = 'Berið aðgangsstýringu að IP Address';
+$lang['web_access_control_apply_mac'] = 'Berið Access Control MAC Address';
+$lang['web_access_control_ctrl_click'] = '(CTRL-smelltu til margar vali)';
+$lang['web_access_control_day_of_week'] = 'Vikudagur';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Aðferð við Identification';
+$lang['web_access_control_list'] = 'Aðgangsstýring lista';
+$lang['web_access_control_name'] = 'heiti';
+$lang['web_access_control_outside'] = 'Úti';
+$lang['web_access_control_outside_range'] = 'Utan takmarkanir tími';
+$lang['web_access_control_priority'] = 'Forgangur';
+$lang['web_access_control_start_time'] = 'Byrjunartími';
+$lang['web_access_control_time_definitions'] = 'Tími dags Skilgreiningar';
+$lang['web_access_control_time_delete_warning'] = 'Viðvörun: allir aðgangsstýring færslur með að þessu sinni skilgreiningu verður eytt.';
+$lang['web_access_control_time_of_day'] = 'Tími dags';
+$lang['web_access_control_time_restriction'] = 'Takmarkanir';
+$lang['web_access_control_time_rule'] = 'Time-af-Day Access Control';
+$lang['web_access_control_type'] = 'Gerð';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'innan';
+$lang['web_access_control_within_range'] = 'Innan takmarkanir tíma';
