ce85a6686e27e7a28e3391bdecbc016ecd8053fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:15:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'সময় সময়কাল যোগ';
+$lang['web_access_control_app_description'] = 'ওয়েব অ্যাক্সেস কন্ট্রোল গ্রুপ বা কম্পিউটার (IP অথবা MAC ঠিকানা) ওয়েব প্রক্সি ব্যবহার করার সময়-এর দিনের ওয়েব এক্সেস বলবত্ করার জন্য একজন প্রশাসক পারবেন.';
+$lang['web_access_control_app_name'] = 'ওয়েব অ্যাক্সেস কন্ট্রোল';
+$lang['web_access_control_apply_group'] = 'গ্রুপের অ্যাক্সেস কন্ট্রোল আবেদন';
+$lang['web_access_control_apply_ip'] = 'আইপি ঠিকানা থেকে অ্যাক্সেস কন্ট্রোল আবেদন';
+$lang['web_access_control_apply_mac'] = 'MAC ঠিকানা অ্যাক্সেস কন্ট্রোল আবেদন';
+$lang['web_access_control_ctrl_click'] = '(একাধিক মান নির্বাচন করার জন্য CTRL-ক্লিক করুন)';
+$lang['web_access_control_day_of_week'] = 'সপ্তাহের দিন';
+$lang['web_access_control_end_time'] = 'শেষ সময়';
+$lang['web_access_control_id_method'] = 'সনাক্তকারী পদ্ধতি';
+$lang['web_access_control_list'] = 'প্রবেশাধিকার নিয়ন্ত্রণ তালিকা';
+$lang['web_access_control_name'] = 'নাম';
+$lang['web_access_control_outside'] = 'বাহিরে';
+$lang['web_access_control_outside_range'] = 'বাইরে সময় বিধিনিষেধ';
+$lang['web_access_control_priority'] = 'অগ্রাধিকার';
+$lang['web_access_control_start_time'] = 'সময় শুরু';
+$lang['web_access_control_time_definitions'] = 'দিবস সংজ্ঞাসমূহ টাইম';
+$lang['web_access_control_time_delete_warning'] = 'সতর্কতা: কোন প্রবেশাধিকার নিয়ন্ত্রণ এই সময় সংজ্ঞা ব্যবহার এন্ট্রি মুছে ফেলা হবে.';
+$lang['web_access_control_time_of_day'] = 'দিনের সময়';
+$lang['web_access_control_time_restriction'] = 'সীমাবদ্ধতা';
+$lang['web_access_control_time_rule'] = 'টাইম অফ ডে অ্যাক্সেস কন্ট্রোল';
+$lang['web_access_control_type'] = 'আদর্শ';
+$lang['web_access_control_web_access_control'] = 'ওয়েব অ্যাক্সেস কন্ট্রোল';
+$lang['web_access_control_within'] = 'মধ্যে';
+$lang['web_access_control_within_range'] = 'সময় নিষেধাজ্ঞা';
