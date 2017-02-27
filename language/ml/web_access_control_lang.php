e31af8da789a89a7ec55aa222e9ab13c985f0db7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:20:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'സമയം കാലയളവ് ചേർക്കുക';
+$lang['web_access_control_app_description'] = 'വെബ് ആക്സസ്സ് കണ്ട്രോൾ ഗ്രൂപ്പുകൾ അല്ലെങ്കിൽ കമ്പ്യൂട്ടറുകൾ സമയം-ന്റെ ഇന്നു വെബ് ആക്സസ്സ് (IP അല്ലെങ്കിൽ മാക് വിലാസം) വെബ് പ്രോക്സി ഉപയോഗിച്ച് നടപ്പിലാക്കാൻ ഒരു അഡ്മിനിസ്ട്രേറ്റർ അനുവദിക്കുന്നു.';
+$lang['web_access_control_app_name'] = 'വെബ് ആക്സസ്സ് നിയന്ത്രണ';
+$lang['web_access_control_apply_group'] = 'ഗ്രൂപ്പ് ആക്സസ് ചെയ്യുന്നത് നിയന്ത്രിക്കാൻ പ്രയോഗിക്കുക';
+$lang['web_access_control_apply_ip'] = 'ഐപി വിലാസം അക്സസ് കൺട്രോൾ പ്രയോഗിക്കുക';
+$lang['web_access_control_apply_mac'] = 'മാക് വിലാസത്തിൽ ആക്സസ് കൺട്രോൾ പ്രയോഗിക്കുക';
+$lang['web_access_control_ctrl_click'] = '(ഒന്നിലധികം തിരഞ്ഞെടുപ്പുകളെ CTRL-ക്ലിക്ക്)';
+$lang['web_access_control_day_of_week'] = 'ആഴ്ചയിലെ ദിവസം';
+$lang['web_access_control_end_time'] = 'അവസാന സമയം';
+$lang['web_access_control_id_method'] = 'ഐഡന്റിഫിക്കേഷൻ രീതി';
+$lang['web_access_control_list'] = 'പ്രവേശന നിയന്ത്രണ ലിസ്റ്റ്';
+$lang['web_access_control_name'] = 'പേര്';
+$lang['web_access_control_outside'] = 'പുറത്ത്';
+$lang['web_access_control_outside_range'] = 'സമയം നിയന്ത്രണങ്ങൾ പുറത്ത്';
+$lang['web_access_control_priority'] = 'മുൻഗണന';
+$lang['web_access_control_start_time'] = 'ആരംഭ സമയം';
+$lang['web_access_control_time_definitions'] = 'ദിവസം നിർവചനങ്ങൾ സമയം';
+$lang['web_access_control_time_delete_warning'] = 'മുന്നറിയിപ്പ്: ഈ സമയം നിർവചനം ഉപയോഗിച്ച് ഏത് പ്രവേശന നിയന്ത്രണ എൻട്രികൾ ഇല്ലാതാക്കും.';
+$lang['web_access_control_time_of_day'] = 'ഇന്നത്തെ സമയം';
+$lang['web_access_control_time_restriction'] = 'നിയന്ത്രണവുമായി';
+$lang['web_access_control_time_rule'] = 'സമയം-ഓഫ്-ഡേ ആക്സസ്സ് കണ്ട്രോള്';
+$lang['web_access_control_type'] = 'ടൈപ്പ് ചെയ്യുക';
+$lang['web_access_control_web_access_control'] = 'വെബ് ആക്സസ്സ് നിയന്ത്രണ';
+$lang['web_access_control_within'] = 'നുള്ളിൽ';
+$lang['web_access_control_within_range'] = 'സമയം പരിമിതികൾ';
