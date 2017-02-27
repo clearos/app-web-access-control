d5cbb46d4f5683cff2c6059c92cd681ffe161b10
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:24:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Idagdag Time Period';
+$lang['web_access_control_app_description'] = 'Web Access Control ay nagpapahintulot sa isang administrator upang ipatupad oras-ng-araw web access sa mga grupo o mga computer (IP o MAC address) gamit ang web proxy.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Ilapat Access Control sa Group';
+$lang['web_access_control_apply_ip'] = 'Ilapat Access Control sa IP Address';
+$lang['web_access_control_apply_mac'] = 'Ilapat Access Control sa MAC Address';
+$lang['web_access_control_ctrl_click'] = '(CTRL click para sa maramihang mga seleksyon)';
+$lang['web_access_control_day_of_week'] = 'Araw sa isang linggo';
+$lang['web_access_control_end_time'] = 'Oras ng Pagtatapos';
+$lang['web_access_control_id_method'] = 'Paraan ng Identification';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'pangalan';
+$lang['web_access_control_outside'] = 'sa labas';
+$lang['web_access_control_outside_range'] = 'Sa labas paghihigpit oras';
+$lang['web_access_control_priority'] = 'karapatang mauna';
+$lang['web_access_control_start_time'] = 'Start Time';
+$lang['web_access_control_time_definitions'] = 'Oras ng Araw Kahulugan';
+$lang['web_access_control_time_delete_warning'] = 'Babala: ang anumang mga entry access control gamit ang time definition ay tatanggalin.';
+$lang['web_access_control_time_of_day'] = 'Oras ng Araw';
+$lang['web_access_control_time_restriction'] = 'pagbabawal';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'uri';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Sa loob ng';
+$lang['web_access_control_within_range'] = 'Sa loob ng oras paghihigpit';
