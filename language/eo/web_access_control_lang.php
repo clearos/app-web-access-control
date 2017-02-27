537776bece917de216ea48920b45d2756e226007
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:16:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Aldoni Tempo Periodo';
+$lang['web_access_control_app_description'] = 'TTT Aliro Kontrolo permesas administranto observigi tempo-de-tago retejo aliro al grupoj aŭ komputiloj (IP aŭ MAC) uzante la TTT proxy.';
+$lang['web_access_control_app_name'] = 'TTT Aliro Kontrolo';
+$lang['web_access_control_apply_group'] = 'Apliki Aliro Kontrolo Group';
+$lang['web_access_control_apply_ip'] = 'Apliki Aliro Kontrolo IP adreso';
+$lang['web_access_control_apply_mac'] = 'Apliki Aliro Kontrolo al MAC Adreso';
+$lang['web_access_control_ctrl_click'] = '(CTRL-klaki por multnombraj selektadoj)';
+$lang['web_access_control_day_of_week'] = 'Tago de Semajno';
+$lang['web_access_control_end_time'] = 'fine Tempo';
+$lang['web_access_control_id_method'] = 'Metodo de Identigo';
+$lang['web_access_control_list'] = 'Aliro Kontrolo Listo';
+$lang['web_access_control_name'] = 'nomo';
+$lang['web_access_control_outside'] = 'ekster';
+$lang['web_access_control_outside_range'] = 'Ekster tempo limigoj';
+$lang['web_access_control_priority'] = 'prioritato';
+$lang['web_access_control_start_time'] = 'Start Time';
+$lang['web_access_control_time_definitions'] = 'Tempo de Tago Difinoj';
+$lang['web_access_control_time_delete_warning'] = 'Averto: neniu aliro kontrolo enskriboj uzante tiun tempon difino estos forigita.';
+$lang['web_access_control_time_of_day'] = 'Tempo de Tago';
+$lang['web_access_control_time_restriction'] = 'limigo';
+$lang['web_access_control_time_rule'] = 'Tempo-de-tago Aliro Kontrolo';
+$lang['web_access_control_type'] = 'tipo';
+$lang['web_access_control_web_access_control'] = 'TTT Aliro Kontrolo';
+$lang['web_access_control_within'] = 'ene';
+$lang['web_access_control_within_range'] = 'Ene tempo limigoj';
