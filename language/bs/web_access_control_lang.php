f9e725885f549586057294b0c4debe4dcbfde65d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:15:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Dodaj vremenski period';
+$lang['web_access_control_app_description'] = 'Web Access Control omogućava administratoru da sprovede vrijeme-of-day web pristup grupama ili kompjutera (IP ili MAC adresa) pomoću web proxy.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Nanesite Kontrola pristupa Group';
+$lang['web_access_control_apply_ip'] = 'Nanesite Kontrola pristupa na IP adresa';
+$lang['web_access_control_apply_mac'] = 'Nanesite Kontrola pristupa MAC adresa';
+$lang['web_access_control_ctrl_click'] = '(CTRL klik za višestruki odabir)';
+$lang['web_access_control_day_of_week'] = 'Dan Tjedan';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Način identifikacije';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'ime';
+$lang['web_access_control_outside'] = 'van';
+$lang['web_access_control_outside_range'] = 'Izvan vremena ograničenja';
+$lang['web_access_control_priority'] = 'prioritet';
+$lang['web_access_control_start_time'] = 'Vrijeme početka';
+$lang['web_access_control_time_definitions'] = 'Doba dana Definicije';
+$lang['web_access_control_time_delete_warning'] = 'Upozorenje: sve stavke kontrola pristupa pomoću ovog puta definicija će biti izbrisani.';
+$lang['web_access_control_time_of_day'] = 'Vrijeme dana';
+$lang['web_access_control_time_restriction'] = 'ograničenje';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'tip';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'u';
+$lang['web_access_control_within_range'] = 'U okviru vremenskih ograničenja';
