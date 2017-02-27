15d253c552dd4d950761b30938ae1982e0460213
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:19:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'რჩეულებში დროის';
+$lang['web_access_control_app_description'] = 'ვებ წვდომის კონტროლის საშუალებას აძლევს ადმინისტრატორს აღასრულოს დრო-of-day ვებ ხელმისაწვდომობის ჯგუფების ან კომპიუტერი (IP და MAC მისამართი) გამოყენებით ვებ მარიონეტული.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'მიმართვა წვდომის კონტროლის ჯგუფი';
+$lang['web_access_control_apply_ip'] = 'მიმართვა წვდომის კონტროლის IP მისამართი';
+$lang['web_access_control_apply_mac'] = 'მიმართვა წვდომის კონტროლის to MAC მისამართი';
+$lang['web_access_control_ctrl_click'] = '(CTRL დააწკაპუნეთ მრავალჯერადი არჩევანი)';
+$lang['web_access_control_day_of_week'] = 'კვირის დღე';
+$lang['web_access_control_end_time'] = 'დროის დასასრული';
+$lang['web_access_control_id_method'] = 'მეთოდი საიდენტიფიკაციო';
+$lang['web_access_control_list'] = 'წვდომის კონტროლის სია';
+$lang['web_access_control_name'] = 'სახელი';
+$lang['web_access_control_outside'] = 'გარეთ';
+$lang['web_access_control_outside_range'] = 'გარეთ დროის შეზღუდვა';
+$lang['web_access_control_priority'] = 'პრიორიტეტი';
+$lang['web_access_control_start_time'] = 'დაწყების დრო';
+$lang['web_access_control_time_definitions'] = 'დღის დრო განმარტებები';
+$lang['web_access_control_time_delete_warning'] = 'გაფრთხილება: დაშვების კონტროლის ჩანაწერების გამოყენებით ამ დროს განმარტება წაიშლება.';
+$lang['web_access_control_time_of_day'] = 'დღის მონაკვეთი';
+$lang['web_access_control_time_restriction'] = 'შეზღუდვა';
+$lang['web_access_control_time_rule'] = 'დრო-of-Day წვდომის კონტროლის';
+$lang['web_access_control_type'] = 'ტიპი';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'ფარგლებში';
+$lang['web_access_control_within_range'] = 'ფარგლებში დროის შეზღუდვა';
