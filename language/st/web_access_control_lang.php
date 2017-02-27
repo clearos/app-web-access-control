24bba810a3b5ed0d319bf3fead972712e3ac9901
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:23:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Add Time Nako';
+$lang['web_access_control_app_description'] = 'Web Access Control e lumella mohlokomeli oa ho tiisa nako ea letsatsi le le leng ho web phihlello ho lihlopha kapa lik&#39;homphieutha (IP kapa mac aterese) sebedisa web vouta.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Etsa Access Control ho Group';
+$lang['web_access_control_apply_ip'] = 'Etsa Access Control ho IP Address';
+$lang['web_access_control_apply_mac'] = 'Etsa Access Control ho mac Address';
+$lang['web_access_control_ctrl_click'] = '(Ctrl Penya bakeng bakhethoa multiple)';
+$lang['web_access_control_day_of_week'] = 'Letsatsi la Week';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Mokhoa ona oa ho boitsebahatso';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'Name';
+$lang['web_access_control_outside'] = 'ka ntle ho';
+$lang['web_access_control_outside_range'] = 'lithibelo ka ntle nako';
+$lang['web_access_control_priority'] = 'etelletsa pele';
+$lang['web_access_control_start_time'] = 'Start Time';
+$lang['web_access_control_time_definitions'] = 'Nako ea Letsatsi Ditlhaloso';
+$lang['web_access_control_time_delete_warning'] = 'Temoso ena: leha e le efe nyako phihlello ya taolo ya ho sebedisa tlhaloso ena nako tla hlakolwa.';
+$lang['web_access_control_time_of_day'] = 'Nako ea Letsatsi';
+$lang['web_access_control_time_restriction'] = 'thibeloe ke';
+$lang['web_access_control_time_rule'] = 'Time of-Day Access Control';
+$lang['web_access_control_type'] = 'Type';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'matsatsing a';
+$lang['web_access_control_within_range'] = 'Matsatsing a nako e lithibelo';
