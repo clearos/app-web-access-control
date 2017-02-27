4c26dd3c92babbb7101d8cd14795e5a002ef5ccb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:17:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'In Zeitraum';
+$lang['web_access_control_app_description'] = 'Web Access Control ermöglicht es einem Administrator Time-of-day Web-Zugriff auf Gruppen oder Computern (IP- oder MAC-Adresse) mit Hilfe des Web-Proxy zu erzwingen.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Wenden Sie Access Control-Gruppe';
+$lang['web_access_control_apply_ip'] = 'Wenden Sie Access Control-IP-Adresse';
+$lang['web_access_control_apply_mac'] = 'Wenden Sie Access Control MAC-Adresse';
+$lang['web_access_control_ctrl_click'] = '(CTRL-Klick für eine Mehrfachauswahl)';
+$lang['web_access_control_day_of_week'] = 'Wochentag';
+$lang['web_access_control_end_time'] = 'Endzeit';
+$lang['web_access_control_id_method'] = 'Verfahren zur Identifizierung';
+$lang['web_access_control_list'] = 'Zugriffskontrollliste';
+$lang['web_access_control_name'] = 'Name';
+$lang['web_access_control_outside'] = 'Draußen';
+$lang['web_access_control_outside_range'] = 'Außerhalb Zeitbeschränkungen';
+$lang['web_access_control_priority'] = 'Priorität';
+$lang['web_access_control_start_time'] = 'Startzeit';
+$lang['web_access_control_time_definitions'] = 'Tageszeit Definitionen';
+$lang['web_access_control_time_delete_warning'] = 'Achtung: alle Zugriffssteuerungseinträge diesmal Definition wird gelöscht.';
+$lang['web_access_control_time_of_day'] = 'Uhrzeit';
+$lang['web_access_control_time_restriction'] = 'Beschränkung';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'Art';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Innerhalb';
+$lang['web_access_control_within_range'] = 'Innerhalb von Zeitbeschränkungen';
