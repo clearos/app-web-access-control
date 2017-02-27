7556ccc4293453396c70090dbc152556239da25f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:20:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Додај Временски период';
+$lang['web_access_control_app_description'] = 'Веб контрола на пристап им овозможува на администраторот да изврши време-на-ден веб пристап на групи или компјутери (IP адреса или MAC адреса) со користење на веб прокси.';
+$lang['web_access_control_app_name'] = 'Контрола за пристап на интернет';
+$lang['web_access_control_apply_group'] = 'Спроведување на контрола на пристап на група';
+$lang['web_access_control_apply_ip'] = 'Спроведување на контрола на пристап на IP адреса';
+$lang['web_access_control_apply_mac'] = 'Спроведување на контрола на пристап до MAC адреса';
+$lang['web_access_control_ctrl_click'] = '(Ctrl-клик на повеќе избори)';
+$lang['web_access_control_day_of_week'] = 'Ден од неделата';
+$lang['web_access_control_end_time'] = 'крајот на времето';
+$lang['web_access_control_id_method'] = 'Начинот на идентификација';
+$lang['web_access_control_list'] = 'На листата за контрола на пристап';
+$lang['web_access_control_name'] = 'име';
+$lang['web_access_control_outside'] = 'Надвор';
+$lang['web_access_control_outside_range'] = 'Надвор временски ограничувања';
+$lang['web_access_control_priority'] = 'приоритет';
+$lang['web_access_control_start_time'] = 'почеток Време';
+$lang['web_access_control_time_definitions'] = 'Време од денот Дефиниции';
+$lang['web_access_control_time_delete_warning'] = 'Предупредување: сите записи за контрола на пристап користење на оваа дефиниција време ќе бидат избришани.';
+$lang['web_access_control_time_of_day'] = 'Време од денот';
+$lang['web_access_control_time_restriction'] = 'ограничување';
+$lang['web_access_control_time_rule'] = 'Време-на-ден за контрола на пристап';
+$lang['web_access_control_type'] = 'тип';
+$lang['web_access_control_web_access_control'] = 'Контрола за пристап на интернет';
+$lang['web_access_control_within'] = 'Во рамките';
+$lang['web_access_control_within_range'] = 'Во границите на времето';
