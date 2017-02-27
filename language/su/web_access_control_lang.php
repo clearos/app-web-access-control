b83e03050509ccadfb68a489ef2bbeed40241065
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:23:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Tambah Time Periode';
+$lang['web_access_control_app_description'] = 'Web Access Control ngamungkinkeun kuncén ngalaksanakeun waktu-of-poé aksés web ka grup atawa komputer (IP atawa alamat Mac) ngagunakeun proxy web.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Panawaran Access Control ka Group';
+$lang['web_access_control_apply_ip'] = 'Panawaran Access Control ka IP Alamat';
+$lang['web_access_control_apply_mac'] = 'Panawaran Access Control ka Mac Alamat';
+$lang['web_access_control_ctrl_click'] = '(Ctrl-klik pikeun sababaraha selections)';
+$lang['web_access_control_day_of_week'] = 'Poé Minggu';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Metoda Identification';
+$lang['web_access_control_list'] = 'Access Control Daptar';
+$lang['web_access_control_name'] = 'nami';
+$lang['web_access_control_outside'] = 'di luar';
+$lang['web_access_control_outside_range'] = 'larangan waktu di luar';
+$lang['web_access_control_priority'] = 'prioritas';
+$lang['web_access_control_start_time'] = 'mimitian Time';
+$lang['web_access_control_time_definitions'] = 'Waktu Poé Watesan';
+$lang['web_access_control_time_delete_warning'] = 'Perhatosan: nu mana wae nu éntri kontrol aksés ngagunakeun harti waktu ieu bakal dipupus.';
+$lang['web_access_control_time_of_day'] = 'Waktu Poé';
+$lang['web_access_control_time_restriction'] = 'wangenan';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'ngetik';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'jero';
+$lang['web_access_control_within_range'] = 'Dina larangan waktu';
