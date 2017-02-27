a611d0c2275e2d8d05696685a77103375591c328
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:14:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'إضافة الفترة الزمنية';
+$lang['web_access_control_app_description'] = 'التحكم في الوصول على شبكة الإنترنت تسمح للمسؤولين لفرض وقت من اليوم الوصول إلى شبكة الإنترنت إلى مجموعات أو أجهزة الكمبيوتر (IP أو عنوان MAC) باستخدام الوكيل على شبكة الإنترنت.';
+$lang['web_access_control_app_name'] = 'التحكم في الوصول على شبكة الإنترنت';
+$lang['web_access_control_apply_group'] = 'تطبيق التحكم في الوصول إلى المجموعة';
+$lang['web_access_control_apply_ip'] = 'تطبيق التحكم في الوصول إلى عنوان IP';
+$lang['web_access_control_apply_mac'] = 'تطبيق التحكم في الوصول إلى عنوان MAC';
+$lang['web_access_control_ctrl_click'] = '(CTRL مزدوجا فوق لتحديدات متعددة)';
+$lang['web_access_control_day_of_week'] = 'يوم من الأسبوع';
+$lang['web_access_control_end_time'] = 'وقت النهاية';
+$lang['web_access_control_id_method'] = 'طريقة تحديد';
+$lang['web_access_control_list'] = 'قائمة نظام الدخول';
+$lang['web_access_control_name'] = 'اسم';
+$lang['web_access_control_outside'] = 'في الخارج';
+$lang['web_access_control_outside_range'] = 'قيود زمنية الخارجية';
+$lang['web_access_control_priority'] = 'أفضلية';
+$lang['web_access_control_start_time'] = 'وقت البدء';
+$lang['web_access_control_time_definitions'] = 'الوقت من يوم تعاريف';
+$lang['web_access_control_time_delete_warning'] = 'تحذير: أي إدخالات التحكم بالوصول باستخدام هذا التعريف الوقت سيتم حذف.';
+$lang['web_access_control_time_of_day'] = 'وقت اليوم';
+$lang['web_access_control_time_restriction'] = 'تقييد';
+$lang['web_access_control_time_rule'] = 'الوقت من يوم التحكم في الوصول';
+$lang['web_access_control_type'] = 'اكتب';
+$lang['web_access_control_web_access_control'] = 'التحكم في الوصول على شبكة الإنترنت';
+$lang['web_access_control_within'] = 'في غضون';
+$lang['web_access_control_within_range'] = 'ضمن قيود زمنية';
