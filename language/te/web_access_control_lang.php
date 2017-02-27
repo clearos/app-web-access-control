59ba9b392f9bfd67e2c350e6ae712350a623c57f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:24:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'సమయ వ్యవధి జోడించండి';
+$lang['web_access_control_app_description'] = 'వెబ్ యాక్సెస్ కంట్రోల్ సమూహాలు లేదా వెబ్ ప్రాక్సీ ఉపయోగించి కంప్యూటర్లు (IP లేదా MAC చిరునామా) కు టైమ్ ఆఫ్ రోజుల వెబ్ యాక్సెస్ అమలుపరచే నిర్వాహకుడు అనుమతిస్తుంది.';
+$lang['web_access_control_app_name'] = 'వెబ్ యాక్సెస్ కంట్రోల్';
+$lang['web_access_control_apply_group'] = 'గ్రూప్ యాక్సెస్ కంట్రోల్ వర్తించు';
+$lang['web_access_control_apply_ip'] = 'IP అడ్రస్ యాక్సెస్ కంట్రోల్ వర్తించు';
+$lang['web_access_control_apply_mac'] = 'MAC చిరునామా యాక్సెస్ కంట్రోల్ వర్తించు';
+$lang['web_access_control_ctrl_click'] = '(బహుళ ఎంపికలు CTRL-క్లిక్ చేయండి)';
+$lang['web_access_control_day_of_week'] = 'వీక్ డే';
+$lang['web_access_control_end_time'] = 'ముగింపు సమయం';
+$lang['web_access_control_id_method'] = 'గుర్తింపు విధానం';
+$lang['web_access_control_list'] = 'ప్రాప్తి నియంత్రణా జాబితా';
+$lang['web_access_control_name'] = 'పేరు';
+$lang['web_access_control_outside'] = 'బయట';
+$lang['web_access_control_outside_range'] = 'బయట సమయ పరిమితుల';
+$lang['web_access_control_priority'] = 'ప్రాధాన్యత';
+$lang['web_access_control_start_time'] = 'ప్రారంభ సమయం';
+$lang['web_access_control_time_definitions'] = 'డే నిర్వచనాలు సమయం';
+$lang['web_access_control_time_delete_warning'] = 'హెచ్చరిక: ఈ సమయంలో నిర్వచనం ఉపయోగించి ఏదైనా యాక్సెస్ నియంత్రణ ఎంట్రీలు తొలగించబడుతుంది.';
+$lang['web_access_control_time_of_day'] = 'రోజులో సమయం';
+$lang['web_access_control_time_restriction'] = 'పరిమితి';
+$lang['web_access_control_time_rule'] = 'టైమ్ ఆఫ్ డే యాక్సెస్ కంట్రోల్';
+$lang['web_access_control_type'] = 'రకం';
+$lang['web_access_control_web_access_control'] = 'వెబ్ యాక్సెస్ కంట్రోల్';
+$lang['web_access_control_within'] = 'లోపల';
+$lang['web_access_control_within_range'] = 'సమయ పరిమితుల లోపల';
