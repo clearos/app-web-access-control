60497d733ddca8dd4575d60cc31bc93ad62d4d71
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:20:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Pridėti laikotarpis';
+$lang['web_access_control_app_description'] = 'Web Access "valdymo funkcija administratoriui vykdyti laiko iš dienos interneto prieigą prie grupių ar kompiuterius (IP arba MAC adresą), naudojant interneto proxy.';
+$lang['web_access_control_app_name'] = 'Interneto prieigos kontrolės';
+$lang['web_access_control_apply_group'] = 'Taikyti Access Control į grupę';
+$lang['web_access_control_apply_ip'] = 'Taikyti Access Control IP adresas';
+$lang['web_access_control_apply_mac'] = 'Taikyti Access Control MAC adresas';
+$lang['web_access_control_ctrl_click'] = '(CTRL spustelėkite kelis pasirinkimus)';
+$lang['web_access_control_day_of_week'] = 'Savaitės diena';
+$lang['web_access_control_end_time'] = 'pabaigos laikas';
+$lang['web_access_control_id_method'] = 'Identifikavimo metodas';
+$lang['web_access_control_list'] = 'Prieigos kontrolės sąrašas';
+$lang['web_access_control_name'] = 'vardas';
+$lang['web_access_control_outside'] = 'už';
+$lang['web_access_control_outside_range'] = 'Už laiko apribojimai';
+$lang['web_access_control_priority'] = 'Prioritetas';
+$lang['web_access_control_start_time'] = 'Pradžios laikas';
+$lang['web_access_control_time_definitions'] = 'Dienų laikas Apibrėžimai';
+$lang['web_access_control_time_delete_warning'] = 'Įspėjimas: bet kokie prieigos kontrolės įrašai, naudojantys šį laiko apibrėžimas bus ištrinta.';
+$lang['web_access_control_time_of_day'] = 'Dienos laikas';
+$lang['web_access_control_time_restriction'] = 'apribojimas';
+$lang['web_access_control_time_rule'] = 'Laikas-of-Day Access Control';
+$lang['web_access_control_type'] = 'tipas';
+$lang['web_access_control_web_access_control'] = 'Interneto prieigos kontrolės';
+$lang['web_access_control_within'] = 'per';
+$lang['web_access_control_within_range'] = 'Per laiko apribojimai';
