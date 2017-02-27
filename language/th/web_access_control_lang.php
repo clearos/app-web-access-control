36cedc3153b03f41f91da458da834a254cdaf783
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:24:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'เพิ่มช่วงเวลา';
+$lang['web_access_control_app_description'] = 'Web Access Control ช่วยให้ผู้ดูแลระบบเพื่อบังคับใช้ช่วงเวลาของวันการเข้าถึงเว็บที่เป็นกลุ่มหรือคอมพิวเตอร์ (IP หรือที่อยู่ MAC) โดยใช้พร็อกซี่เว็บ';
+$lang['web_access_control_app_name'] = 'การควบคุมการเข้าถึงเว็บ';
+$lang['web_access_control_apply_group'] = 'สมัครควบคุมการเข้าถึงกลุ่ม';
+$lang['web_access_control_apply_ip'] = 'สมัครการควบคุมการเข้าถึงไปยังที่อยู่ IP';
+$lang['web_access_control_apply_mac'] = 'สมัครควบคุมการเข้าถึงที่อยู่ MAC';
+$lang['web_access_control_ctrl_click'] = '(กดปุ่ม CTRL คลิกสำหรับการเลือกหลาย)';
+$lang['web_access_control_day_of_week'] = 'วันของสัปดาห์';
+$lang['web_access_control_end_time'] = 'เวลาสิ้นสุด';
+$lang['web_access_control_id_method'] = 'วิธีการระบุ';
+$lang['web_access_control_list'] = 'รายการควบคุมการเข้าถึง';
+$lang['web_access_control_name'] = 'ชื่อ';
+$lang['web_access_control_outside'] = 'ด้านนอก';
+$lang['web_access_control_outside_range'] = 'ข้อ จำกัด ของเวลานอก';
+$lang['web_access_control_priority'] = 'ลำดับความสำคัญ';
+$lang['web_access_control_start_time'] = 'เวลาเริ่มต้น';
+$lang['web_access_control_time_definitions'] = 'เวลาของคำนิยามวัน';
+$lang['web_access_control_time_delete_warning'] = 'คำเตือน: รายการใด ๆ การควบคุมการเข้าถึงใช้คำนิยามในครั้งนี้จะถูกลบออก';
+$lang['web_access_control_time_of_day'] = 'เวลาของวัน';
+$lang['web_access_control_time_restriction'] = 'การ จำกัด';
+$lang['web_access_control_time_rule'] = 'เวลาของวันการควบคุมการเข้าถึง';
+$lang['web_access_control_type'] = 'ชนิด';
+$lang['web_access_control_web_access_control'] = 'การควบคุมการเข้าถึงเว็บ';
+$lang['web_access_control_within'] = 'ภายใน';
+$lang['web_access_control_within_range'] = 'ภายในเวลา จำกัด';
