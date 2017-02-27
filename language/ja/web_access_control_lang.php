0bcffd0acd6109515fa5e7739687bb6d51bf1ddc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:19:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = '時間帯を追加します。';
+$lang['web_access_control_app_description'] = 'Webアクセス制御は、グループやWebプロキシを使用しているコンピュータ（IPまたはMACアドレス）への時刻ウェブアクセスを強制するには、管理者を許可します。';
+$lang['web_access_control_app_name'] = 'Webアクセスの制御';
+$lang['web_access_control_apply_group'] = 'グループへのアクセス制御を適用します';
+$lang['web_access_control_apply_ip'] = 'IPアドレスへのアクセス制御を適用します';
+$lang['web_access_control_apply_mac'] = 'MACアドレスにアクセス制御を適用します。';
+$lang['web_access_control_ctrl_click'] = '（複数選択のためにCtrlを押しながらクリック）';
+$lang['web_access_control_day_of_week'] = '曜日';
+$lang['web_access_control_end_time'] = '終了時間';
+$lang['web_access_control_id_method'] = '同定法';
+$lang['web_access_control_list'] = 'アクセス制御リスト';
+$lang['web_access_control_name'] = '名';
+$lang['web_access_control_outside'] = '外側';
+$lang['web_access_control_outside_range'] = '外の時間制限';
+$lang['web_access_control_priority'] = '優先';
+$lang['web_access_control_start_time'] = '始まる時間';
+$lang['web_access_control_time_definitions'] = 'デイ定義の時間';
+$lang['web_access_control_time_delete_warning'] = '注意：この時間の定義を使用して任意のアクセス制御エントリが削除されます。';
+$lang['web_access_control_time_of_day'] = '時刻';
+$lang['web_access_control_time_restriction'] = '制限';
+$lang['web_access_control_time_rule'] = '時間帯のアクセス制御';
+$lang['web_access_control_type'] = 'タイプ';
+$lang['web_access_control_web_access_control'] = 'Webアクセスの制御';
+$lang['web_access_control_within'] = '以内';
+$lang['web_access_control_within_range'] = '時間制限内';
