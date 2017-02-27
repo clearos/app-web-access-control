9c4ca97eaeb21facfb58915beb6037d4527e7075
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:23:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Dodaj Časovno obdobje';
+$lang['web_access_control_app_description'] = 'Web Access Control omogoča skrbniku, da uveljavi čas od dneva spletni dostop do skupine ali računalnikov (IP ali MAC naslov) z uporabo web proxy.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Uporabi Access Control skupini';
+$lang['web_access_control_apply_ip'] = 'Uporabi Access Control IP naslov';
+$lang['web_access_control_apply_mac'] = 'Uporabi Access Control za MAC naslov';
+$lang['web_access_control_ctrl_click'] = '(CTRL-klik za več izborov)';
+$lang['web_access_control_day_of_week'] = 'Dan v tednu';
+$lang['web_access_control_end_time'] = 'končni čas';
+$lang['web_access_control_id_method'] = 'Metoda identifikacije';
+$lang['web_access_control_list'] = 'Seznam za nadzor dostopa';
+$lang['web_access_control_name'] = 'ime';
+$lang['web_access_control_outside'] = 'zunaj';
+$lang['web_access_control_outside_range'] = 'Zunanje časovne omejitve';
+$lang['web_access_control_priority'] = 'Prednost';
+$lang['web_access_control_start_time'] = 'Začetni čas';
+$lang['web_access_control_time_definitions'] = 'Čas Dan Definicije';
+$lang['web_access_control_time_delete_warning'] = 'Opozorilo: vsi vnosov za nadzor dostopa, ki uporabljajo to časovno opredelitev bodo izbrisani.';
+$lang['web_access_control_time_of_day'] = 'Čas dneva';
+$lang['web_access_control_time_restriction'] = 'omejitev';
+$lang['web_access_control_time_rule'] = 'Čas-of-Day Access Control';
+$lang['web_access_control_type'] = 'Vrsta';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'znotraj';
+$lang['web_access_control_within_range'] = 'V časovnih omejitev';
