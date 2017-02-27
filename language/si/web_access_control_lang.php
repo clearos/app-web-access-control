e10f11beebd5df21612cc89a0e7ca6020c3aac78
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:23:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'කාල සීමාව එක් කරන්න';
+$lang['web_access_control_app_description'] = 'වෙබ් ප්රවේශ පාලන වෙබ් ප්රොක්සි භාවිතා කණ්ඩායම් හෝ පරිගණක කාලය-of-දින අන්තර්ජාලය (IP හෝ MAC Address) ක්රියාත්මක කිරීමට පරිපාලක හැක.';
+$lang['web_access_control_app_name'] = 'වෙබ් ප්රවේශ පාලන';
+$lang['web_access_control_apply_group'] = 'සමූහ ප්රවේශ පාලන යොමු කරන්න';
+$lang['web_access_control_apply_ip'] = 'IP ලිපිනය වෙත ප්රවේශය පාලනය කරන්න';
+$lang['web_access_control_apply_mac'] = 'MAC ලිපිනය වෙත ප්රවේශය පාලනය කරන්න';
+$lang['web_access_control_ctrl_click'] = '(බහු තේරීම් සඳහා Ctrl-ක්ලික් කරන්න)';
+$lang['web_access_control_day_of_week'] = 'සතියේ දින';
+$lang['web_access_control_end_time'] = 'අවසන් කාල';
+$lang['web_access_control_id_method'] = 'හදුනා ගැනීම ක්රමය';
+$lang['web_access_control_list'] = 'ප්රවේශ පාලක ලැයිස්තුව';
+$lang['web_access_control_name'] = 'නම';
+$lang['web_access_control_outside'] = 'පිටත';
+$lang['web_access_control_outside_range'] = 'කාලය සීමා පිටත';
+$lang['web_access_control_priority'] = 'ප්රමුඛ';
+$lang['web_access_control_start_time'] = 'ආරම්භක වේලාව';
+$lang['web_access_control_time_definitions'] = 'දින අර්ථ කථනය සඳහා ගතවන කාලය';
+$lang['web_access_control_time_delete_warning'] = 'අවවාදයයි: මෙම කාලය අර්ථදැක්වීම භාවිතා ඕනෑම ප්රවේශ පාලනය සටහන් ඇතුළත් කිරීම් මකා දමනු ඇත.';
+$lang['web_access_control_time_of_day'] = 'දිනය වේලාව';
+$lang['web_access_control_time_restriction'] = 'සීමා';
+$lang['web_access_control_time_rule'] = 'කාලය-of-දින ප්රවේශ පාලන';
+$lang['web_access_control_type'] = 'වර්ගය';
+$lang['web_access_control_web_access_control'] = 'වෙබ් ප්රවේශ පාලන';
+$lang['web_access_control_within'] = 'තුළ';
+$lang['web_access_control_within_range'] = 'කාලය සීමා තුළ';
