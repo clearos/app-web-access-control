0cad334dc9dbcf7c31bdc0466cf0b7e0ed8c4e69
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:21:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Tambahkan Tempoh Masa';
+$lang['web_access_control_app_description'] = 'Kawalan Akses Web membolehkan pentadbir untuk menguatkuasakan akses web masa-of-hari untuk kumpulan atau komputer (IP atau alamat MAC) menggunakan proksi web.';
+$lang['web_access_control_app_name'] = 'Kawalan Akses Web';
+$lang['web_access_control_apply_group'] = 'Memohon Kawalan Akses kepada Kumpulan';
+$lang['web_access_control_apply_ip'] = 'Memohon Kawalan Akses kepada Alamat IP';
+$lang['web_access_control_apply_mac'] = 'Memohon Kawalan Akses kepada MAC Alamat';
+$lang['web_access_control_ctrl_click'] = '(CTRL-klik untuk pelbagai pilihan)';
+$lang['web_access_control_day_of_week'] = 'Hari dalam seminggu';
+$lang['web_access_control_end_time'] = 'Masa tamat';
+$lang['web_access_control_id_method'] = 'Kaedah Pengenalan';
+$lang['web_access_control_list'] = 'Senarai Kawalan Akses';
+$lang['web_access_control_name'] = 'Nama';
+$lang['web_access_control_outside'] = 'Di luar';
+$lang['web_access_control_outside_range'] = 'sekatan masa di luar';
+$lang['web_access_control_priority'] = 'keutamaan';
+$lang['web_access_control_start_time'] = 'Masa mula';
+$lang['web_access_control_time_definitions'] = 'Masa Hari Definisi';
+$lang['web_access_control_time_delete_warning'] = 'Amaran: mana-mana penyertaan kawalan akses menggunakan definisi kali ini akan dipadam.';
+$lang['web_access_control_time_of_day'] = 'Masa Hari';
+$lang['web_access_control_time_restriction'] = 'sekatan';
+$lang['web_access_control_time_rule'] = 'Masa-of-Day Kawalan Akses';
+$lang['web_access_control_type'] = 'Jenis';
+$lang['web_access_control_web_access_control'] = 'Kawalan Akses Web';
+$lang['web_access_control_within'] = 'dalam';
+$lang['web_access_control_within_range'] = 'Dalam sekatan masa';
