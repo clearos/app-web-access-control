3f8dccae4a8e6a91bb8110ac02ff8deb7bd8d22e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:23:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Kuongeza Muda Kipindi';
+$lang['web_access_control_app_description'] = 'Mtandao Access Control inaruhusu msimamizi wa kutekeleza wakati wa siku mtandao upatikanaji wa vikundi au kompyuta (IP au MAC anwani) kwa kutumia mtandao wakala.';
+$lang['web_access_control_app_name'] = 'Mtandao Access Control';
+$lang['web_access_control_apply_group'] = 'Kuomba Access Control kwa Group';
+$lang['web_access_control_apply_ip'] = 'Kuomba Access Control kwa IP ya mitaani';
+$lang['web_access_control_apply_mac'] = 'Kuomba Access Control kwa MAC mitaani';
+$lang['web_access_control_ctrl_click'] = '(CTRL-click kwa uchaguzi nyingi)';
+$lang['web_access_control_day_of_week'] = 'Siku ya Wiki';
+$lang['web_access_control_end_time'] = 'mwisho Time';
+$lang['web_access_control_id_method'] = 'Njia ya Identification';
+$lang['web_access_control_list'] = 'Upatikanaji Orodha Control';
+$lang['web_access_control_name'] = 'jina';
+$lang['web_access_control_outside'] = 'nje';
+$lang['web_access_control_outside_range'] = 'vikwazo nje wakati';
+$lang['web_access_control_priority'] = 'kipaumbele';
+$lang['web_access_control_start_time'] = 'Start Muda';
+$lang['web_access_control_time_definitions'] = 'Wakati wa Siku Ufafanuzi';
+$lang['web_access_control_time_delete_warning'] = 'Onyo: yoyote entries upatikanaji wa kudhibiti kwa kutumia hii ufafanuzi wakati itakuwa ilifutwa.';
+$lang['web_access_control_time_of_day'] = 'Wakati wa Siku';
+$lang['web_access_control_time_restriction'] = 'kizuizi';
+$lang['web_access_control_time_rule'] = 'Muda-ya-Day Access Control';
+$lang['web_access_control_type'] = 'aina';
+$lang['web_access_control_web_access_control'] = 'Mtandao Access Control';
+$lang['web_access_control_within'] = 'ndani';
+$lang['web_access_control_within_range'] = 'Ndani ya vikwazo wakati';
