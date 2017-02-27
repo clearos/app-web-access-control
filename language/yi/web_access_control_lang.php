189a2411cb38198ccae35f8480e45975391bcba4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:25:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'לייג צו Time פּעריאָד';
+$lang['web_access_control_app_description'] = 'וועב אַקסעס קאָנטראָל אַלאַוז אַ אַדמיניסטראַטאָר צו דורכפירן צייַט-פון-טאָג וועב צוטריט צו גרופּעס אָדער קאָמפּיוטערס (יפּ אָדער מעק אַדרעס) ניצן די וועב פּראָקסי.';
+$lang['web_access_control_app_name'] = 'וועב אַקסעס קאָנטראָל';
+$lang['web_access_control_apply_group'] = 'צולייגן אַקסעס קאָנטראָל צו גרופּע';
+$lang['web_access_control_apply_ip'] = 'צולייגן אַקסעס קאָנטראָל צו יפּ אַדרעס';
+$lang['web_access_control_apply_mac'] = 'צולייגן אַקסעס קאָנטראָל צו מעק אַדרעס';
+$lang['web_access_control_ctrl_click'] = '(קטרל-גיט פֿאַר קייפל סאַלעקשאַנז)';
+$lang['web_access_control_day_of_week'] = 'טאָג פון וואָך';
+$lang['web_access_control_end_time'] = 'סוף צייט';
+$lang['web_access_control_id_method'] = 'אופֿן פון לעגיטימאַציע';
+$lang['web_access_control_list'] = 'אַקסעס קאָנטראָל ליסטע';
+$lang['web_access_control_name'] = 'נאָמען';
+$lang['web_access_control_outside'] = 'אַרויס';
+$lang['web_access_control_outside_range'] = 'אַרויס צייַט ריסטריקשאַנז';
+$lang['web_access_control_priority'] = 'בילכערקייַט';
+$lang['web_access_control_start_time'] = 'אָנהייב צייט';
+$lang['web_access_control_time_definitions'] = 'צייַט פון טאָג זוך';
+$lang['web_access_control_time_delete_warning'] = 'ווארענונג: קיין צוטריט קאָנטראָל איינסן ניצן דעם צייַט דעפֿיניציע וועט ווערן אויסגעמעקט.';
+$lang['web_access_control_time_of_day'] = 'צייַט פון טאָג';
+$lang['web_access_control_time_restriction'] = 'רעסטריקטיאָן';
+$lang['web_access_control_time_rule'] = 'צייט-פון-טאָג אַקסעס קאָנטראָל';
+$lang['web_access_control_type'] = 'טיפּ';
+$lang['web_access_control_web_access_control'] = 'וועב אַקסעס קאָנטראָל';
+$lang['web_access_control_within'] = 'ין';
+$lang['web_access_control_within_range'] = 'ין צייַט ריסטריקשאַנז';
