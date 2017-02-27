8ce8a54ad6589295257fe9af803bfd69d18d543f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:15:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Ավելացնել ժամանակահատվածը';
+$lang['web_access_control_app_description'] = 'Վեբ Access Control թույլ է տալիս ադմինիստրատորին իրականացնել ժամանակատար է օր ցանցային մուտք դեպի խմբերի կամ համակարգիչների (IP կամ MAC հասցեն), օգտագործելով վեբ վստահված անձը:';
+$lang['web_access_control_app_name'] = 'Վեբ Access Control';
+$lang['web_access_control_apply_group'] = 'Դիմել Access Control խմբի';
+$lang['web_access_control_apply_ip'] = 'Դիմել Access Control է IP հասցե';
+$lang['web_access_control_apply_mac'] = 'Դիմել Access Control է MAC հասցեն';
+$lang['web_access_control_ctrl_click'] = '(CTRL սեղմեք բազմակի ընտրության)';
+$lang['web_access_control_day_of_week'] = 'Օր Շաբաթվա';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Մեթոդ նույնականացման';
+$lang['web_access_control_list'] = 'Մուտքի վերահսկման ցուցակ';
+$lang['web_access_control_name'] = 'անուն';
+$lang['web_access_control_outside'] = 'Outside';
+$lang['web_access_control_outside_range'] = 'Դրսի անգամ սահմանափակումները';
+$lang['web_access_control_priority'] = 'առաջնություն';
+$lang['web_access_control_start_time'] = 'Start Time';
+$lang['web_access_control_time_definitions'] = 'Ժամանակը օրվա Սահմանումներ';
+$lang['web_access_control_time_delete_warning'] = 'Զգուշացում: որեւէ մուտքի հսկման գրառում, օգտագործելով այս անգամ սահմանմանը կջնջվի:';
+$lang['web_access_control_time_of_day'] = 'Օրվա ժամանակ';
+$lang['web_access_control_time_restriction'] = 'սահմանափակում';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'Տիպ';
+$lang['web_access_control_web_access_control'] = 'Վեբ Access Control';
+$lang['web_access_control_within'] = 'ընթացքում';
+$lang['web_access_control_within_range'] = 'Ժամկետներում սահմանափակումների';
