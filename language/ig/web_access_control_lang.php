1b10ad364ed7ca4466ad3c1ea97386f2862600bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:18:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Tinye Time Oge';
+$lang['web_access_control_app_description'] = 'Web Access Control-enye ohere onye nchịkwa na-amanye ha oge-nke-ụbọchị web ohere dị iche iche ma ọ bụ kọmputa (IP ma ọ bụ Mac adreesị) iji web zighachi akwụkwọ nnọchiteanya.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Tee Access Control ka Group';
+$lang['web_access_control_apply_ip'] = 'Tee Access Control ka Adreesị IP';
+$lang['web_access_control_apply_mac'] = 'Tee Access Control ka Mac Address';
+$lang['web_access_control_ctrl_click'] = '(CTRL-pịa maka otutu selections)';
+$lang['web_access_control_day_of_week'] = 'Day nke Week';
+$lang['web_access_control_end_time'] = 'ọgwụgwụ Time';
+$lang['web_access_control_id_method'] = 'Method nke Identification';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'aha';
+$lang['web_access_control_outside'] = 'n&#39;èzí';
+$lang['web_access_control_outside_range'] = 'N&#39;èzí oge mgbochi';
+$lang['web_access_control_priority'] = 'Ibu ụzọ';
+$lang['web_access_control_start_time'] = 'Malite Time';
+$lang['web_access_control_time_definitions'] = 'Time of Day akọwa';
+$lang['web_access_control_time_delete_warning'] = 'Amụma: bụla ohere akara Ndenye na iji oge definition ga-ehichapụ.';
+$lang['web_access_control_time_of_day'] = 'Time of Day';
+$lang['web_access_control_time_restriction'] = 'mmachi';
+$lang['web_access_control_time_rule'] = 'Time-nke-Day Access Control';
+$lang['web_access_control_type'] = 'ụdị';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'n&#39;ime';
+$lang['web_access_control_within_range'] = 'N&#39;ime oge mgbochi';
