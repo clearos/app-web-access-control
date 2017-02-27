f272d514daf0e7aaaf95aa8863278a25aa8606ab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:18:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'समय अवधि जोड़ें';
+$lang['web_access_control_app_description'] = 'वेब एक्सेस कंट्रोल समूहों या कंप्यूटर (आईपी या मैक पते) वेब प्रॉक्सी का उपयोग करने के लिए समय-के-दिन वेब का उपयोग लागू करने के लिए एक प्रशासक की अनुमति देता है।';
+$lang['web_access_control_app_name'] = 'वेब एक्सेस कंट्रोल';
+$lang['web_access_control_apply_group'] = 'समूह के लिए उपयोग नियंत्रण लागू करें';
+$lang['web_access_control_apply_ip'] = 'आईपी ​​पता करने के लिए प्रवेश नियंत्रण लागू करें';
+$lang['web_access_control_apply_mac'] = 'मैक पता करने के लिए प्रवेश नियंत्रण लागू करें';
+$lang['web_access_control_ctrl_click'] = '(एकाधिक चयन के लिए CTRL क्लिक करें)';
+$lang['web_access_control_day_of_week'] = 'सप्ताह के दिन';
+$lang['web_access_control_end_time'] = 'अंतिम समय';
+$lang['web_access_control_id_method'] = 'पहचान की विधि';
+$lang['web_access_control_list'] = 'कंट्रोल सूची को खोलो';
+$lang['web_access_control_name'] = 'नाम';
+$lang['web_access_control_outside'] = 'बाहर';
+$lang['web_access_control_outside_range'] = 'बाहर समय प्रतिबंध';
+$lang['web_access_control_priority'] = 'प्राथमिकता';
+$lang['web_access_control_start_time'] = 'समय शुरू';
+$lang['web_access_control_time_definitions'] = 'डे परिभाषाओं का समय';
+$lang['web_access_control_time_delete_warning'] = 'चेतावनी: किसी भी अभिगम नियंत्रण इस समय परिभाषा का उपयोग प्रविष्टियों को हटा दिया जाएगा।';
+$lang['web_access_control_time_of_day'] = 'दिन का समय';
+$lang['web_access_control_time_restriction'] = 'सीमा';
+$lang['web_access_control_time_rule'] = 'समय के दिन प्रवेश नियंत्रण';
+$lang['web_access_control_type'] = 'प्रकार';
+$lang['web_access_control_web_access_control'] = 'वेब एक्सेस कंट्रोल';
+$lang['web_access_control_within'] = 'अंदर';
+$lang['web_access_control_within_range'] = 'समय प्रतिबंध के भीतर';
