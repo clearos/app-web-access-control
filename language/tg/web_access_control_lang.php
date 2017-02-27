3e60396f16c5d69b7648603e20c1576ac5879df9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:24:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Иловаи Мӯҳлати Time';
+$lang['web_access_control_app_description'] = 'Назорати Дастрасӣ ба Интернет имкон медиҳад, бо як аз мудирон барои таҳкими риояи вақт-аз-рӯз дастрасӣ ба интернет гурӯҳҳо ё компютер (IP ё суроғаи МАЗМ) бо истифода аз прокси веб.';
+$lang['web_access_control_app_name'] = 'Назорати Дастрасӣ ба Интернет';
+$lang['web_access_control_apply_group'] = 'Apply назорати Дастрасӣ ба гурӯҳи';
+$lang['web_access_control_apply_ip'] = 'Apply назорати Дастрасӣ ба IP Нишонӣ';
+$lang['web_access_control_apply_mac'] = 'Apply назорати Дастрасӣ ба МАЗМ Суроға';
+$lang['web_access_control_ctrl_click'] = '(CTRL-ро пахш кунед барои интихоби якчанд)';
+$lang['web_access_control_day_of_week'] = 'Рӯзи ҳафтаи';
+$lang['web_access_control_end_time'] = 'Time End';
+$lang['web_access_control_id_method'] = 'Усули муайян';
+$lang['web_access_control_list'] = 'Рӯйхати назорати Access';
+$lang['web_access_control_name'] = 'ном';
+$lang['web_access_control_outside'] = 'берун аз';
+$lang['web_access_control_outside_range'] = 'Маҳдудиятҳои вақти Берун';
+$lang['web_access_control_priority'] = 'Афзалияти';
+$lang['web_access_control_start_time'] = 'Time Оғоз';
+$lang['web_access_control_time_definitions'] = 'Вақт Рӯзи Мафњумњо';
+$lang['web_access_control_time_delete_warning'] = 'Огоҳӣ: ҳар вурудоти идоракунии дастрасӣ бо истифода аз ин таърифи вақт тоза карда мешавад.';
+$lang['web_access_control_time_of_day'] = 'Time Рӯзи';
+$lang['web_access_control_time_restriction'] = 'маҳдудият';
+$lang['web_access_control_time_rule'] = 'Вақт-аз-рӯзи назорати Access';
+$lang['web_access_control_type'] = 'Намуди';
+$lang['web_access_control_web_access_control'] = 'Назорати Дастрасӣ ба Интернет';
+$lang['web_access_control_within'] = 'дар ҳудуди';
+$lang['web_access_control_within_range'] = 'Дар давоми замони маҳдудиятҳо';
