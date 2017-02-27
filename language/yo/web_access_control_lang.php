78c39ccfa6106b235667b879ba4096c10b73593d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:25:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Fi Time Akoko';
+$lang['web_access_control_app_description'] = 'Web Access Iṣakoso faye gba ohun IT lati mu lagabara akoko-ti-ọjọ ayelujara wiwọle si awọn ẹgbẹ tabi awọn kọmputa (IP tabi Mac adirẹsi) lilo awọn aaye ayelujara aṣoju.';
+$lang['web_access_control_app_name'] = 'Web Access Iṣakoso';
+$lang['web_access_control_apply_group'] = 'Waye Access Iṣakoso to Group';
+$lang['web_access_control_apply_ip'] = 'Waye Access Iṣakoso to IP Adirẹsi';
+$lang['web_access_control_apply_mac'] = 'Waye Access Iṣakoso to Mac adirẹsi';
+$lang['web_access_control_ctrl_click'] = '(Konturolu-tẹ fun ọpọ aṣayan)';
+$lang['web_access_control_day_of_week'] = 'Day of Osu';
+$lang['web_access_control_end_time'] = 'Ipari Time';
+$lang['web_access_control_id_method'] = 'Ọna ti Identification';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'Name';
+$lang['web_access_control_outside'] = 'ita';
+$lang['web_access_control_outside_range'] = 'Ita akoko awọn ihamọ';
+$lang['web_access_control_priority'] = 'ni ayo';
+$lang['web_access_control_start_time'] = 'Bẹrẹ Time';
+$lang['web_access_control_time_definitions'] = 'Akoko ti Day Itumo';
+$lang['web_access_control_time_delete_warning'] = 'Ìkìlọ: eyikeyi wiwọle iṣakoso awọn titẹ sii nipa lilo akoko yi definition yoo paarẹ.';
+$lang['web_access_control_time_of_day'] = 'Akoko ti Day';
+$lang['web_access_control_time_restriction'] = 'hihamọ';
+$lang['web_access_control_time_rule'] = 'Time-ti-Day Access Iṣakoso';
+$lang['web_access_control_type'] = 'iru';
+$lang['web_access_control_web_access_control'] = 'Web Access Iṣakoso';
+$lang['web_access_control_within'] = 'laarin';
+$lang['web_access_control_within_range'] = 'Laarin akoko awọn ihamọ';
