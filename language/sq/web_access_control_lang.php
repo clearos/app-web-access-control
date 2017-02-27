cb223bb246f995335f96acadeaeb6dc821cc0532
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:14:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Shto periudhë kohore';
+$lang['web_access_control_app_description'] = 'Web Access Control lejon një administrator për të zbatuar koha-e-ditë aksesit të internetit në grupe apo kompjuter (IP ose adresa MAC) duke përdorur web proxy.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Aplikoni Access Control Grupit';
+$lang['web_access_control_apply_ip'] = 'Aplikoni Access Control për IP Address';
+$lang['web_access_control_apply_mac'] = 'Aplikoni Access Control për MAC Adresa';
+$lang['web_access_control_ctrl_click'] = '(CTRL-klikoni për zgjedhje të shumëfishta)';
+$lang['web_access_control_day_of_week'] = 'Dita e Javës';
+$lang['web_access_control_end_time'] = 'Fundi';
+$lang['web_access_control_id_method'] = 'Metoda e identifikimit';
+$lang['web_access_control_list'] = 'Access Lista e kontrollit';
+$lang['web_access_control_name'] = 'emër';
+$lang['web_access_control_outside'] = 'jashtë';
+$lang['web_access_control_outside_range'] = 'Kufizimet kohore jashtë';
+$lang['web_access_control_priority'] = 'prioritet';
+$lang['web_access_control_start_time'] = 'Koha e nisjes';
+$lang['web_access_control_time_definitions'] = 'Koha e Përkufizime Ditës';
+$lang['web_access_control_time_delete_warning'] = 'Warning: ndonjë mesazh të librit e kontrollit të qasjes duke përdorur këtë përkufizim kohor do të fshihen.';
+$lang['web_access_control_time_of_day'] = 'Koha e Ditës';
+$lang['web_access_control_time_restriction'] = 'kufizimi';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'lloj';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'brenda';
+$lang['web_access_control_within_range'] = 'Brenda kufizimeve kohore';
