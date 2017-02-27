81700b48bedcb915b48148a4befd185dcb600158
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:25:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Thêm Thời gian Thời gian';
+$lang['web_access_control_app_description'] = 'Web Access Control cho phép người quản trị để thực thi thời gian của ngày truy cập web cho các nhóm hay máy tính (IP hoặc địa chỉ MAC) bằng cách sử dụng web proxy.';
+$lang['web_access_control_app_name'] = 'Kiểm soát truy cập web';
+$lang['web_access_control_apply_group'] = 'Áp dụng Access Control để Nhóm';
+$lang['web_access_control_apply_ip'] = 'Áp dụng điều khiển truy cập tới địa chỉ IP';
+$lang['web_access_control_apply_mac'] = 'Áp dụng điều khiển truy cập tới địa chỉ MAC';
+$lang['web_access_control_ctrl_click'] = '(Ctrl-nhấp chọn nhiều lựa chọn)';
+$lang['web_access_control_day_of_week'] = 'Ngày trong tuần';
+$lang['web_access_control_end_time'] = 'Thời gian kết thúc';
+$lang['web_access_control_id_method'] = 'Phương pháp Xác định';
+$lang['web_access_control_list'] = 'Danh sách điều khiển truy cập';
+$lang['web_access_control_name'] = 'Tên';
+$lang['web_access_control_outside'] = 'Ở ngoài';
+$lang['web_access_control_outside_range'] = 'hạn chế thời gian bên ngoài';
+$lang['web_access_control_priority'] = 'Sự ưu tiên';
+$lang['web_access_control_start_time'] = 'Thời gian bắt đầu';
+$lang['web_access_control_time_definitions'] = 'Thời gian của ngày Định nghĩa';
+$lang['web_access_control_time_delete_warning'] = 'Cảnh báo: bất kỳ mục kiểm soát truy cập sử dụng định nghĩa thời gian này sẽ bị xóa.';
+$lang['web_access_control_time_of_day'] = 'Thời gian trong ngày';
+$lang['web_access_control_time_restriction'] = 'Sự hạn chế';
+$lang['web_access_control_time_rule'] = 'Time-of-ngày Access Control';
+$lang['web_access_control_type'] = 'Kiểu';
+$lang['web_access_control_web_access_control'] = 'Kiểm soát truy cập web';
+$lang['web_access_control_within'] = 'ở trong';
+$lang['web_access_control_within_range'] = 'Trong giới hạn thời gian';
