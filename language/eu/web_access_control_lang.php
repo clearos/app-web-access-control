2683ec7cde8a396ceb2618e573749a4386b5085c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:15:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Gehitu Time Epea';
+$lang['web_access_control_app_description'] = 'Web Access Control administratzaile bat denbora-of-egunean web talde edo ordenagailuak (IP edo MAC helbidea) web proxy erabiliz sarbidea betearazteko ahalbidetzen.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Aplikatu Access Control Group';
+$lang['web_access_control_apply_ip'] = 'Aplikatu Access Control IP Helbidea';
+$lang['web_access_control_apply_mac'] = 'Aplikatu Access Control den MAC helbidea';
+$lang['web_access_control_ctrl_click'] = '(CTRL-klikatu Hautaketa anitzak erabiliz)';
+$lang['web_access_control_day_of_week'] = 'Asteko eguneko';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Identifikazio metodoa';
+$lang['web_access_control_list'] = 'Sarbideko kontrol-zerrenda';
+$lang['web_access_control_name'] = 'izena';
+$lang['web_access_control_outside'] = 'Kanpoaldean';
+$lang['web_access_control_outside_range'] = 'Kanpoaldean denbora murrizketak';
+$lang['web_access_control_priority'] = 'Lehentasuna';
+$lang['web_access_control_start_time'] = 'Hasiera ordua';
+$lang['web_access_control_time_definitions'] = 'Eguna Definitions Time';
+$lang['web_access_control_time_delete_warning'] = 'Abisua: edozein sarbide-kontrol sarrerak denbora definizio hori erabiliz ezabatuko dira.';
+$lang['web_access_control_time_of_day'] = 'Eguna Ordua';
+$lang['web_access_control_time_restriction'] = 'Murrizketa';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'Mota';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Within';
+$lang['web_access_control_within_range'] = 'denbora murrizketak barruan';
