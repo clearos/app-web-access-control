62b4f49043c1632203011da0b7b431f8e56a5496
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:18:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Tambahkan Periode Waktu';
+$lang['web_access_control_app_description'] = 'Web Access Control memungkinkan administrator untuk menegakkan waktu-of-hari akses web untuk kelompok atau komputer (IP atau alamat MAC) menggunakan web proxy.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Terapkan Access Control untuk Grup';
+$lang['web_access_control_apply_ip'] = 'Terapkan Access Control untuk IP Address';
+$lang['web_access_control_apply_mac'] = 'Terapkan Access Control untuk MAC Address';
+$lang['web_access_control_ctrl_click'] = '(CTRL-klik untuk beberapa pilihan)';
+$lang['web_access_control_day_of_week'] = 'Hari dalam seminggu';
+$lang['web_access_control_end_time'] = 'Akhir waktu';
+$lang['web_access_control_id_method'] = 'Metode Identifikasi';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'Nama';
+$lang['web_access_control_outside'] = 'Di luar';
+$lang['web_access_control_outside_range'] = 'batasan waktu di luar';
+$lang['web_access_control_priority'] = 'Prioritas';
+$lang['web_access_control_start_time'] = 'Waktu mulai';
+$lang['web_access_control_time_definitions'] = 'Waktu Hari Definisi';
+$lang['web_access_control_time_delete_warning'] = 'Peringatan: entri kontrol akses menggunakan definisi saat ini akan dihapus.';
+$lang['web_access_control_time_of_day'] = 'Waktu Hari';
+$lang['web_access_control_time_restriction'] = 'Larangan';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'Mengetik';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Dalam';
+$lang['web_access_control_within_range'] = 'Dalam batasan waktu';
