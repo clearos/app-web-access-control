fe98d58378bc952cda188873091bfd7705c1c14c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:25:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Ychwanegu Cyfnod Amser';
+$lang['web_access_control_app_description'] = 'Rheoli Mynediad We yn caniatáu i weinyddwr i orfodi mynediad i&#39;r we amser-o-ddydd i grwpiau neu gyfrifiaduron (IP neu gyfeiriad MAC) gan ddefnyddio&#39;r dirprwy we.';
+$lang['web_access_control_app_name'] = 'Rheoli Mynediad We';
+$lang['web_access_control_apply_group'] = 'Gwneud cais Rheoli Mynediad i Grŵp';
+$lang['web_access_control_apply_ip'] = 'Gwneud cais Rheoli Mynediad at Cyfeiriad IP';
+$lang['web_access_control_apply_mac'] = 'Gwneud cais Rheoli Mynediad i MAC Cyfeiriad';
+$lang['web_access_control_ctrl_click'] = '(CTRL-cliciwch am dewisiadau lluosog)';
+$lang['web_access_control_day_of_week'] = 'Diwrnod Wythnos';
+$lang['web_access_control_end_time'] = 'Amser Gorffen';
+$lang['web_access_control_id_method'] = 'Dull Adnabod';
+$lang['web_access_control_list'] = 'Rhestr Rheoli Mynediad';
+$lang['web_access_control_name'] = 'enw';
+$lang['web_access_control_outside'] = 'Y tu allan';
+$lang['web_access_control_outside_range'] = 'cyfyngiadau amser y tu allan';
+$lang['web_access_control_priority'] = 'blaenoriaeth';
+$lang['web_access_control_start_time'] = 'Amser Cychwyn';
+$lang['web_access_control_time_definitions'] = 'Amser y Diffiniadau Dydd';
+$lang['web_access_control_time_delete_warning'] = 'Rhybudd: Bydd unrhyw gofnodion rheoli mynediad gan ddefnyddio diffiniad y tro hwn yn cael ei ddileu.';
+$lang['web_access_control_time_of_day'] = 'Amser y Dydd';
+$lang['web_access_control_time_restriction'] = 'cyfyngiad';
+$lang['web_access_control_time_rule'] = 'Amser-o-Day Rheoli Mynediad';
+$lang['web_access_control_type'] = 'math';
+$lang['web_access_control_web_access_control'] = 'Rheoli Mynediad We';
+$lang['web_access_control_within'] = 'O fewn';
+$lang['web_access_control_within_range'] = 'O fewn cyfyngiadau amser';
