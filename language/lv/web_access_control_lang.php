9ce2e06ce4a6cb111badc4729ced57ab6a51e600
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:20:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Pievienot Laika periods';
+$lang['web_access_control_app_description'] = 'Web Access Control ļauj administratoram izpildīt laika-of-dienas interneta piekļuvi grupām vai datoriem (IP vai MAC adrese), izmantojot tīmekļa starpniekserveri.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Piesakies Access Control Group';
+$lang['web_access_control_apply_ip'] = 'Piesakies Access Control IP adrese';
+$lang['web_access_control_apply_mac'] = 'Piesakies Access Control MAC adrese';
+$lang['web_access_control_ctrl_click'] = '(CTRL klikšķi vairākām izvēles)';
+$lang['web_access_control_day_of_week'] = 'Nedēļas diena';
+$lang['web_access_control_end_time'] = 'beigu laiks';
+$lang['web_access_control_id_method'] = 'Identifikācijas metodi';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'nosaukums';
+$lang['web_access_control_outside'] = 'ārpus';
+$lang['web_access_control_outside_range'] = 'Ārpus laika ierobežojumi';
+$lang['web_access_control_priority'] = 'prioritāte';
+$lang['web_access_control_start_time'] = 'Sākuma laiks';
+$lang['web_access_control_time_definitions'] = 'Diennakts laiks definīcijas';
+$lang['web_access_control_time_delete_warning'] = 'Brīdinājums: jebkuras piekļuves kontroles ierakstu, izmantojot šo laika definīciju, tiks dzēsti.';
+$lang['web_access_control_time_of_day'] = 'Dienas laiks';
+$lang['web_access_control_time_restriction'] = 'ierobežojums';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'tips';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'laikā';
+$lang['web_access_control_within_range'] = 'Ietvaros laika ierobežojumu';
