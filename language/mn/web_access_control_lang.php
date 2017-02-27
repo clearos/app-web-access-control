ac8002b9981ce321350b0839937fba3d04748117
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:21:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Цаг хугацааг нэмэх';
+$lang['web_access_control_app_description'] = 'Вэб Access Control бүлэг, вэб прокси ашиглаж байгаа компьютер (IP болон MAC хаяг) цаг-ийн тутмын вэб хандалт хэрэгжүүлэх администраторт олгодог.';
+$lang['web_access_control_app_name'] = 'Вэб Хандалтын Хяналт нь';
+$lang['web_access_control_apply_group'] = 'Групп хандах хандалтыг хянах хэрэглэх';
+$lang['web_access_control_apply_ip'] = 'IP хаяг уруу хандах хандалтыг хянахын хэрэглэх';
+$lang['web_access_control_apply_mac'] = 'MAC Address хандах хандалтыг хянах хэрэглэх';
+$lang['web_access_control_ctrl_click'] = '(Олон сонголт нь CTRL дарж)';
+$lang['web_access_control_day_of_week'] = 'Долоо хоногийн өдөр';
+$lang['web_access_control_end_time'] = 'Төгсгөл цаг';
+$lang['web_access_control_id_method'] = 'Тодорхойлох арга';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'нэр';
+$lang['web_access_control_outside'] = 'Гаднын';
+$lang['web_access_control_outside_range'] = 'Гаднын цаг хязгаарлалт';
+$lang['web_access_control_priority'] = 'тэргүүлэх';
+$lang['web_access_control_start_time'] = 'Эхэлсэн цаг';
+$lang['web_access_control_time_definitions'] = 'Өдөр Нэр томъёоны тайлбар цаг';
+$lang['web_access_control_time_delete_warning'] = 'Анхааруулга: энэ удаад тодорхойлолтыг ашиглан ямар ч хандалтын хяналтын оруулгууд нь устгагдах болно.';
+$lang['web_access_control_time_of_day'] = 'Өдрийн цаг';
+$lang['web_access_control_time_restriction'] = 'хязгаарлах';
+$lang['web_access_control_time_rule'] = 'Time-ийн өдрийн хандалт хянах';
+$lang['web_access_control_type'] = 'Санал авах';
+$lang['web_access_control_web_access_control'] = 'Вэб Хандалтын Хяналт нь';
+$lang['web_access_control_within'] = 'дотор';
+$lang['web_access_control_within_range'] = 'цаг хязгаарлалт дотор';
