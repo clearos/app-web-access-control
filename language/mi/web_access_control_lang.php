00e61cb544efe466c83819f3e9a4937cbae30122
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:20:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Tāpiri Time Wā';
+$lang['web_access_control_app_description'] = 'Mana Access Tukutuku taea te kaiwhakahaere ki te uruhi i te uru wā-o-ra tukutuku ki ngā rōpū rorohiko (IP wāhitau MAC ranei) te whakamahi i te takawaenga tukutuku ranei.';
+$lang['web_access_control_app_name'] = 'Mana Access Tukutuku';
+$lang['web_access_control_apply_group'] = 'Anga Mana Access ki Rōpū';
+$lang['web_access_control_apply_ip'] = 'Anga Mana Access ki Wāhitau IP';
+$lang['web_access_control_apply_mac'] = 'Anga Mana Access ki MAC Wāhitau';
+$lang['web_access_control_ctrl_click'] = '(Pāwhiritia-CTRL mō kōwhiringa maha)';
+$lang['web_access_control_day_of_week'] = 'Day o Wiki';
+$lang['web_access_control_end_time'] = 'Wā whakamutunga';
+$lang['web_access_control_id_method'] = 'Tikanga o te Tāutunga';
+$lang['web_access_control_list'] = 'Rārangi Mana Access';
+$lang['web_access_control_name'] = 'ingoa';
+$lang['web_access_control_outside'] = 'waho';
+$lang['web_access_control_outside_range'] = 'here wā waho';
+$lang['web_access_control_priority'] = 'Manakē';
+$lang['web_access_control_start_time'] = 'Wā Tīmata';
+$lang['web_access_control_time_definitions'] = 'Time o Day whakamāramatanga';
+$lang['web_access_control_time_delete_warning'] = 'Whakatūpato: ka tetahi tāurunga mana uru te whakamahi i tenei whakamāramatanga wa e mukua.';
+$lang['web_access_control_time_of_day'] = 'Wā o Day';
+$lang['web_access_control_time_restriction'] = 'Tikanga';
+$lang['web_access_control_time_rule'] = 'Time-o-Day Mana Access';
+$lang['web_access_control_type'] = 'momo';
+$lang['web_access_control_web_access_control'] = 'Mana Access Tukutuku';
+$lang['web_access_control_within'] = 'I roto i';
+$lang['web_access_control_within_range'] = 'I roto i here wā';
