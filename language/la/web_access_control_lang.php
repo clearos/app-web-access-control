bb4b59184b4e845d82218e3070582a6b97efbcec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:19:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Add Tempus Tempus';
+$lang['web_access_control_app_description'] = 'Web access control administratoris rerum patitur tempus risus vel est hodie textus obvius Coetibus (MAC et IP oratio) telam per procuratorem.';
+$lang['web_access_control_app_name'] = 'Web Access Imperium';
+$lang['web_access_control_apply_group'] = 'Ingrediatur ad Access Imperium Group';
+$lang['web_access_control_apply_ip'] = 'Ingrediatur ad IP Access Imperium';
+$lang['web_access_control_apply_mac'] = 'Applicare Access Imperium Allocutio ad MAC';
+$lang['web_access_control_ctrl_click'] = '(Ctrl-Click for multiplex excerpta)';
+$lang['web_access_control_day_of_week'] = 'Week dies';
+$lang['web_access_control_end_time'] = 'End time';
+$lang['web_access_control_id_method'] = 'Ratio cognitionis';
+$lang['web_access_control_list'] = 'Obvius Imperium List';
+$lang['web_access_control_name'] = 'nomine';
+$lang['web_access_control_outside'] = 'extra';
+$lang['web_access_control_outside_range'] = 'Extra tempus restrictiones';
+$lang['web_access_control_priority'] = 'PRAECELLENTIA';
+$lang['web_access_control_start_time'] = 'Satus tempus';
+$lang['web_access_control_time_definitions'] = 'Definitions tempore';
+$lang['web_access_control_time_delete_warning'] = 'Warning: accessus usura Imperium entries hanc definitionem non esse deleta.';
+$lang['web_access_control_time_of_day'] = 'Tempus dies';
+$lang['web_access_control_time_restriction'] = 'Mycobacterium';
+$lang['web_access_control_time_rule'] = 'Obvius Imperium time-of-Day';
+$lang['web_access_control_type'] = 'Type';
+$lang['web_access_control_web_access_control'] = 'Web Access Imperium';
+$lang['web_access_control_within'] = 'in';
+$lang['web_access_control_within_range'] = 'In tempore angustiae';
