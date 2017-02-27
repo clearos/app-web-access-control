fc4ce3d9bcb93a9924c452923c95e904c8d72da8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:23:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Add Time Period';
+$lang['web_access_control_app_description'] = 'Web Access Control u ogolaanaya maamule si uu u dhaqan waqti-of-maalin web ay helaan kooxaha ama kombiyuutarada (IP ama cinwaanka MAC) oo isticmaalaya wakiil internetka.';
+$lang['web_access_control_app_name'] = 'Control Access Web';
+$lang['web_access_control_apply_group'] = 'Codso Control Helitaanka Group';
+$lang['web_access_control_apply_ip'] = 'Codso Control Helitaanka Cinwaanka IP';
+$lang['web_access_control_apply_mac'] = 'Codso Control Helitaanka MAC Address';
+$lang['web_access_control_ctrl_click'] = '(Ctrl-riix for doorashada kala duwan)';
+$lang['web_access_control_day_of_week'] = 'Maalinta Week';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Habka of Aqoonsiga';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'Magaca';
+$lang['web_access_control_outside'] = 'ka baxsan';
+$lang['web_access_control_outside_range'] = 'xayiraad waqti ka baxsan';
+$lang['web_access_control_priority'] = 'Mudnaanta';
+$lang['web_access_control_start_time'] = 'Wakhtiga uu bilaabanayo';
+$lang['web_access_control_time_definitions'] = 'Time of Day Qeexitaanno';
+$lang['web_access_control_time_delete_warning'] = 'Digniin: kasta entries helaan gacanta la isticmaalayo waqti qeexitaankan laga tirtirayaa.';
+$lang['web_access_control_time_of_day'] = 'Time of Day';
+$lang['web_access_control_time_restriction'] = 'Xakamaynta';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'Nooca';
+$lang['web_access_control_web_access_control'] = 'Control Access Web';
+$lang['web_access_control_within'] = 'gudahood';
+$lang['web_access_control_within_range'] = 'xayiraad waqti gudahood';
