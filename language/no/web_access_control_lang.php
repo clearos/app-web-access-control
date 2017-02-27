dd2ca921399be1156862f3d0fbb000eab9bd7e10
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:21:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Legg Tidsperiode';
+$lang['web_access_control_app_description'] = 'Web Access Control lar en administrator for å håndheve time-of-day nettilgang til grupper eller datamaskiner (IP eller MAC-adresse) ved hjelp av web proxy.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Påfør Access Control til konsernet';
+$lang['web_access_control_apply_ip'] = 'Påfør Access Control til IP-adresse';
+$lang['web_access_control_apply_mac'] = 'Påfør Access Control til MAC-adresse';
+$lang['web_access_control_ctrl_click'] = '(CTRL-klikk for å velge flere)';
+$lang['web_access_control_day_of_week'] = 'Ukedag';
+$lang['web_access_control_end_time'] = 'Sluttid';
+$lang['web_access_control_id_method'] = 'Identifikasjonsmetode';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'Navn';
+$lang['web_access_control_outside'] = 'Utenfor';
+$lang['web_access_control_outside_range'] = 'Utenfor tidsbegrensninger';
+$lang['web_access_control_priority'] = 'Prioritet';
+$lang['web_access_control_start_time'] = 'Starttid';
+$lang['web_access_control_time_definitions'] = 'Time of Day Definisjoner';
+$lang['web_access_control_time_delete_warning'] = 'Advarsel: noen adgangskontroll oppføringer ved hjelp av denne tiden definisjonen vil bli slettet.';
+$lang['web_access_control_time_of_day'] = 'Tid på dagen';
+$lang['web_access_control_time_restriction'] = 'Begrensning';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'Type';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Innenfor';
+$lang['web_access_control_within_range'] = 'Innen tidsbegrensninger';
