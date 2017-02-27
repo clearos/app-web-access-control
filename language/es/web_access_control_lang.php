9437c19a568ac106095a8ce29ee8ecfb9104ecce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:23:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Añadir Período de tiempo';
+$lang['web_access_control_app_description'] = 'Control de acceso web permite a un administrador para forzar un acceso web en tiempo-del-día para grupos o equipos (dirección IP o MAC) utilizando el poder de la tela.';
+$lang['web_access_control_app_name'] = 'Control de Acceso Web';
+$lang['web_access_control_apply_group'] = 'Aplicar control de acceso al Grupo';
+$lang['web_access_control_apply_ip'] = 'Aplicar control de acceso a la dirección IP';
+$lang['web_access_control_apply_mac'] = 'Aplicar control de acceso a la dirección MAC';
+$lang['web_access_control_ctrl_click'] = '(Ctrl + clic para seleccionar varias opciones)';
+$lang['web_access_control_day_of_week'] = 'Día de la semana';
+$lang['web_access_control_end_time'] = 'Hora de finalización';
+$lang['web_access_control_id_method'] = 'Método de identificación';
+$lang['web_access_control_list'] = 'Lista de control de acceso';
+$lang['web_access_control_name'] = 'Nombre';
+$lang['web_access_control_outside'] = 'Fuera de';
+$lang['web_access_control_outside_range'] = 'restricciones de tiempo fuera';
+$lang['web_access_control_priority'] = 'Prioridad';
+$lang['web_access_control_start_time'] = 'Hora de inicio';
+$lang['web_access_control_time_definitions'] = 'Hora del día Definiciones';
+$lang['web_access_control_time_delete_warning'] = 'Advertencia: las entradas de control de acceso que utilizan esta definición se procede al borrado.';
+$lang['web_access_control_time_of_day'] = 'Hora del día';
+$lang['web_access_control_time_restriction'] = 'Restricción';
+$lang['web_access_control_time_rule'] = 'La hora del día de Control de Acceso';
+$lang['web_access_control_type'] = 'Tipo';
+$lang['web_access_control_web_access_control'] = 'Control de Acceso Web';
+$lang['web_access_control_within'] = 'Dentro';
+$lang['web_access_control_within_range'] = 'Dentro de las restricciones de tiempo';
