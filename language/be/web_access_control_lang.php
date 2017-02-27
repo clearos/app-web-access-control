8366462d5a1fcf68520782ee493c0dd44002b63c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:15:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Дадаць Перыяд часу';
+$lang['web_access_control_app_description'] = 'Вэб Кантроль доступу дазваляе адміністратару забяспечваць захаванне часу ў дзень вэб-доступ да груп або кампутараў (IP або MAC-адрас), выкарыстоўваючы вэб-проксі.';
+$lang['web_access_control_app_name'] = 'Кантроль Web Access';
+$lang['web_access_control_apply_group'] = 'Прымяніць Кіраванне доступам да групы';
+$lang['web_access_control_apply_ip'] = 'Прымяніць кантроль доступу да IP-адрас';
+$lang['web_access_control_apply_mac'] = 'Прымяніць кантроль доступу да MAC-адрасах';
+$lang['web_access_control_ctrl_click'] = '(CTRL-клік для выбару некалькіх)';
+$lang['web_access_control_day_of_week'] = 'дзень тыдня';
+$lang['web_access_control_end_time'] = 'час заканчэння';
+$lang['web_access_control_id_method'] = 'метад ідэнтыфікацыі';
+$lang['web_access_control_list'] = 'Спіс рэгулявання доступу';
+$lang['web_access_control_name'] = 'імя';
+$lang['web_access_control_outside'] = 'звонку';
+$lang['web_access_control_outside_range'] = 'Знешнія абмежаванні па часе';
+$lang['web_access_control_priority'] = 'прыярытэт';
+$lang['web_access_control_start_time'] = 'Час пачатку';
+$lang['web_access_control_time_definitions'] = 'Час сутак Азначэнні';
+$lang['web_access_control_time_delete_warning'] = 'Увага: любыя элементы кіравання доступам з выкарыстаннем гэтага вызначэння часу будуць выдаленыя.';
+$lang['web_access_control_time_of_day'] = 'час дня';
+$lang['web_access_control_time_restriction'] = 'абмежаванне';
+$lang['web_access_control_time_rule'] = 'Час-Day кантролю доступу';
+$lang['web_access_control_type'] = 'тып';
+$lang['web_access_control_web_access_control'] = 'Кантроль Web Access';
+$lang['web_access_control_within'] = 'ў';
+$lang['web_access_control_within_range'] = 'У часовых абмежаванняў';
