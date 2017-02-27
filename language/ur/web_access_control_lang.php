8e545190a02f4e96a41bb7a00faf6bc6465eb55b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:25:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'وقت کی مدت میں شامل کریں';
+$lang['web_access_control_app_description'] = 'ویب تک رسائی کے کنٹرول کے گروپوں یا کمپیوٹر (IP یا میک ایڈریس) ویب پراکسی کا استعمال کرتے ہوئے کرنے کے لئے دن کے وقت کے ویب تک رسائی کو نافذ کرنے کے ایک منتظم کی اجازت دیتا ہے.';
+$lang['web_access_control_app_name'] = 'ویب تک رسائی کنٹرول';
+$lang['web_access_control_apply_group'] = 'گروپ کی رسائی کنٹرول لگائیں';
+$lang['web_access_control_apply_ip'] = 'IP ایڈریس کو رسائی کنٹرول لگائیں';
+$lang['web_access_control_apply_mac'] = 'MAC ایڈریس کو رسائی کنٹرول لگائیں';
+$lang['web_access_control_ctrl_click'] = '(ایک سے زیادہ انتخاب کے لئے CTRL کلک کریں)';
+$lang['web_access_control_day_of_week'] = 'ہفتہ کے دن';
+$lang['web_access_control_end_time'] = 'آخر وقت';
+$lang['web_access_control_id_method'] = 'شناخت کا طریقہ کار';
+$lang['web_access_control_list'] = 'رسائی کنٹرول فہرست';
+$lang['web_access_control_name'] = 'نام';
+$lang['web_access_control_outside'] = 'باہر';
+$lang['web_access_control_outside_range'] = 'باہر وقت پابندیاں';
+$lang['web_access_control_priority'] = 'ترجیح';
+$lang['web_access_control_start_time'] = 'وقت آغاز';
+$lang['web_access_control_time_definitions'] = 'دن کی تعریف میں سے ٹائم';
+$lang['web_access_control_time_delete_warning'] = 'انتباہ: اس وقت تعریف استعمال کرتے ہوئے کسی بھی ایکسیس کنٹرول کے اندراجات کو خارج کر دیا جائے گا.';
+$lang['web_access_control_time_of_day'] = 'دن کا وقت';
+$lang['web_access_control_time_restriction'] = 'پابندی';
+$lang['web_access_control_time_rule'] = 'وقت کی یوم رسائی کنٹرول';
+$lang['web_access_control_type'] = 'قسم';
+$lang['web_access_control_web_access_control'] = 'ویب تک رسائی کنٹرول';
+$lang['web_access_control_within'] = 'کے اندر اندر';
+$lang['web_access_control_within_range'] = 'وقت پابندیوں کے اندر اندر';
