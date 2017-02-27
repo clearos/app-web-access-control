9ece6c90c18be5a8c49a8171c049c6ce412c72a8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:16:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Kuwonjezera Time Nyengo';
+$lang['web_access_control_app_description'] = 'Web Access Control amalola mkulu kuthandiza nthawi ya tsiku intaneti kupeza magulu kapena kompyuta (IP kapena Mac adiresi) pogwiritsa ntchito intaneti tidzakulowereni.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Ntchito Access Control kwa Gulu';
+$lang['web_access_control_apply_ip'] = 'Ntchito Access Control kuti IP Address';
+$lang['web_access_control_apply_mac'] = 'Ntchito Access Control kuti Mac Address';
+$lang['web_access_control_ctrl_click'] = '(Ctrl Dinani kwa masankhidwe angapo)';
+$lang['web_access_control_day_of_week'] = 'Tsiku la Sabata';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Njira chizindikiro';
+$lang['web_access_control_list'] = 'Access List Control';
+$lang['web_access_control_name'] = 'Name';
+$lang['web_access_control_outside'] = 'kunja';
+$lang['web_access_control_outside_range'] = 'malamulo kunja nthawi';
+$lang['web_access_control_priority'] = 'patsogolo';
+$lang['web_access_control_start_time'] = 'Start Time';
+$lang['web_access_control_time_definitions'] = 'Nthawi ya Tsiku Matanthauzo';
+$lang['web_access_control_time_delete_warning'] = 'Chenjezo: iliyonse zolemba mwayi ulamuliro kugwiritsa ntchito tanthauzo nthawi zichotsedwa.';
+$lang['web_access_control_time_of_day'] = 'Nthawi ya Tsiku';
+$lang['web_access_control_time_restriction'] = 'chiletso';
+$lang['web_access_control_time_rule'] = 'Time-wa-Day Access Control';
+$lang['web_access_control_type'] = 'Type';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Patangotha';
+$lang['web_access_control_within_range'] = 'Mwa malamulo nthawi';
