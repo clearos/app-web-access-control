590972bf3d6478e62bff8541a42ae277a4dfeba7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:18:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Cuir Tréimhse Ama';
+$lang['web_access_control_app_description'] = 'Ceadaíonn Rialú Rochtana Gréasáin riarthóir go chéile-de-lae rochtana gréasáin do ghrúpaí nó ríomhairí (IP nó seoladh MAC) ag baint úsáide as an seachfhreastalaí gréasáin fhorfheidhmiú.';
+$lang['web_access_control_app_name'] = 'Rialú Rochtana gréasáin';
+$lang['web_access_control_apply_group'] = 'Cuir iarratas isteach Rialú Rochtain Ghrúpa';
+$lang['web_access_control_apply_ip'] = 'Cuir iarratas isteach Rialú Rochtain Seoladh IP';
+$lang['web_access_control_apply_mac'] = 'Cuir iarratas isteach Rialú Rochtain MAC Seoladh';
+$lang['web_access_control_ctrl_click'] = '(CTRL-cliceáil do roghanna il)';
+$lang['web_access_control_day_of_week'] = 'Lá na Seachtaine';
+$lang['web_access_control_end_time'] = 'Am deireadh';
+$lang['web_access_control_id_method'] = 'An modh Aithint';
+$lang['web_access_control_list'] = 'Rochtain Liosta Rialaithe';
+$lang['web_access_control_name'] = 'Ainm';
+$lang['web_access_control_outside'] = 'lasmuigh';
+$lang['web_access_control_outside_range'] = 'srianta ama Lasmuigh';
+$lang['web_access_control_priority'] = 'tosaíocht';
+$lang['web_access_control_start_time'] = 'Am tosaithe';
+$lang['web_access_control_time_definitions'] = 'Am na Sainmhínithe Lae';
+$lang['web_access_control_time_delete_warning'] = 'Rabhadh: Beidh aon iontrálacha rialaithe rochtana ag baint úsáide as an sainmhíniú ama a scriosadh.';
+$lang['web_access_control_time_of_day'] = 'Am an Lae';
+$lang['web_access_control_time_restriction'] = 'Srian';
+$lang['web_access_control_time_rule'] = 'Am-de-Day Rialú Rochtana';
+$lang['web_access_control_type'] = 'cineál';
+$lang['web_access_control_web_access_control'] = 'Rialú Rochtana gréasáin';
+$lang['web_access_control_within'] = 'laistigh';
+$lang['web_access_control_within_range'] = 'Laistigh srianta ama';
