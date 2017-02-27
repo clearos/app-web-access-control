11bf9572b1670d16e973b0f6e35659d9fbe823a8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:19:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Уақыт кезеңін қосу';
+$lang['web_access_control_app_description'] = 'Web Access Control веб проксиді қолдану топтарға немесе компьютерлерге уақыт-күні Web Access (IP немесе MAC мекенжайы) орындалуын қамтамасыз ету әкімшісіне береді.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Тобына кіру басқару жағыңыз';
+$lang['web_access_control_apply_ip'] = 'IP-мекен-жай Access Control жағыңыз';
+$lang['web_access_control_apply_mac'] = 'MAC Жолдауы Access Control жағыңыз';
+$lang['web_access_control_ctrl_click'] = '(Бірнеше Таңдау CTRL-басыңыз)';
+$lang['web_access_control_day_of_week'] = 'Аптаның күнін';
+$lang['web_access_control_end_time'] = 'аяқтау уақыты';
+$lang['web_access_control_id_method'] = 'Сәйкестендіру әдісі';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'ат';
+$lang['web_access_control_outside'] = 'сыртында';
+$lang['web_access_control_outside_range'] = 'уақыты шектеулер Сыртта';
+$lang['web_access_control_priority'] = 'басымдық';
+$lang['web_access_control_start_time'] = 'Басталу уақыты';
+$lang['web_access_control_time_definitions'] = 'Күні Анықтамалар уақыты';
+$lang['web_access_control_time_delete_warning'] = 'Ескерту: бұл жолы анықтамасын пайдаланып кез келген кіруді бақылау жазбалары жойылады.';
+$lang['web_access_control_time_of_day'] = 'Тәулік уақытын';
+$lang['web_access_control_time_restriction'] = 'шектеу';
+$lang['web_access_control_time_rule'] = 'Уақыт-күніне Access Control';
+$lang['web_access_control_type'] = 'түрі';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'ішінде';
+$lang['web_access_control_within_range'] = 'уақыты шектеулер шеңберінде';
