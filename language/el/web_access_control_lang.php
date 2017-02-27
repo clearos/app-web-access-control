9565f1e53afb0f3294c94f169288e797d13f4e34
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:17:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Προσθέστε Χρονική περίοδος';
+$lang['web_access_control_app_description'] = 'Ελέγχου Web Access επιτρέπει σε έναν διαχειριστή να επιβάλει την ώρα της ημέρας πρόσβαση στο διαδίκτυο σε ομάδες ή υπολογιστές (διεύθυνση IP ή MAC) με τη χρήση του web proxy.';
+$lang['web_access_control_app_name'] = 'Έλεγχος πρόσβασης στο Web';
+$lang['web_access_control_apply_group'] = 'Εφαρμογή Ελέγχου Πρόσβασης στον Όμιλο';
+$lang['web_access_control_apply_ip'] = 'Εφαρμογή ελέγχου πρόσβασης σε IP Address';
+$lang['web_access_control_apply_mac'] = 'Εφαρμογή ελέγχου πρόσβασης σε MAC Address';
+$lang['web_access_control_ctrl_click'] = '(CTRL-κλικ για πολλαπλές επιλογές)';
+$lang['web_access_control_day_of_week'] = 'Μερα της ΕΒΔΟΜΑΔΑΣ';
+$lang['web_access_control_end_time'] = 'Τέλος χρόνου';
+$lang['web_access_control_id_method'] = 'Μέθοδος Αναγνώρισης';
+$lang['web_access_control_list'] = 'Λίστα Ελέγχου Πρόσβασης';
+$lang['web_access_control_name'] = 'Ονομα';
+$lang['web_access_control_outside'] = 'Εξω απο';
+$lang['web_access_control_outside_range'] = 'Έξω από τους περιορισμούς του χρόνου';
+$lang['web_access_control_priority'] = 'Προτεραιότητα';
+$lang['web_access_control_start_time'] = 'Ωρα έναρξης';
+$lang['web_access_control_time_definitions'] = 'Ώρα της ημέρας Ορισμοί';
+$lang['web_access_control_time_delete_warning'] = 'Προειδοποίηση: τυχόν καταχωρήσεις ελέγχου πρόσβασης που χρησιμοποιούν αυτό τον ορισμό του χρόνου θα διαγραφούν.';
+$lang['web_access_control_time_of_day'] = 'Ωρα της ημέρας';
+$lang['web_access_control_time_restriction'] = 'Περιορισμός';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Ελέγχου Πρόσβασης';
+$lang['web_access_control_type'] = 'Τύπος';
+$lang['web_access_control_web_access_control'] = 'Έλεγχος πρόσβασης στο Web';
+$lang['web_access_control_within'] = 'Στα πλαίσια';
+$lang['web_access_control_within_range'] = 'Μέσα σε χρονικούς περιορισμούς';
