97dadec22996d0c11df46bf6b514e21fc69a4335
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:17:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Ajoute Peryòd Tan';
+$lang['web_access_control_app_description'] = 'Web dwa ak kontwòl pèmèt yon administratè aplike tan-a-jou aksè entènèt nan gwoup oswa òdinatè (IP oswa MAC adrès) lè l sèvi avèk prokurasyon wèb la.';
+$lang['web_access_control_app_name'] = 'Web dwa ak kontwòl';
+$lang['web_access_control_apply_group'] = 'Aplike dwa ak kontwòl nan gwoup';
+$lang['web_access_control_apply_ip'] = 'Aplike dwa ak kontwòl nan adrès IP';
+$lang['web_access_control_apply_mac'] = 'Aplike dwa ak kontwòl nan Mac adrès';
+$lang['web_access_control_ctrl_click'] = '(Ctrl-klike sou pou seleksyon miltip)';
+$lang['web_access_control_day_of_week'] = 'Jou nan Semèn';
+$lang['web_access_control_end_time'] = 'fini';
+$lang['web_access_control_id_method'] = 'Metòd nan Idantifikasyon';
+$lang['web_access_control_list'] = 'Aksè Lis kontwòl';
+$lang['web_access_control_name'] = 'Non';
+$lang['web_access_control_outside'] = 'Deyò';
+$lang['web_access_control_outside_range'] = 'Deyò restriksyon tan';
+$lang['web_access_control_priority'] = 'Priyorite';
+$lang['web_access_control_start_time'] = 'kòmanse Lè';
+$lang['web_access_control_time_definitions'] = 'Tan nan Definisyon jou';
+$lang['web_access_control_time_delete_warning'] = 'Avètisman: pral nenpòt antre aksè kontwòl lè l sèvi avèk sa a definisyon tan ap efase.';
+$lang['web_access_control_time_of_day'] = 'Lè nan jounen';
+$lang['web_access_control_time_restriction'] = 'restriksyon sou';
+$lang['web_access_control_time_rule'] = 'Tan-of-jou dwa ak kontwòl';
+$lang['web_access_control_type'] = 'Kalite';
+$lang['web_access_control_web_access_control'] = 'Web dwa ak kontwòl';
+$lang['web_access_control_within'] = 'nan';
+$lang['web_access_control_within_range'] = 'Nan restriksyon tan';
