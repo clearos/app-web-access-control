1bd6e70822483b6bd7a19f4389cace245bd78306
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:22:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'اضافه کردن دوره زمانی';
+$lang['web_access_control_app_description'] = 'کنترل دسترسی به وب سایت اجازه می دهد تا یک مدیر برای به اجرا درآوردن زمان از روز دسترسی به وب به گروه ها یا رایانه (IP یا آدرس MAC) با استفاده از پروکسی وب.';
+$lang['web_access_control_app_name'] = 'کنترل دسترسی به وب سایت';
+$lang['web_access_control_apply_group'] = 'اعمال کنترل دسترسی به گروه';
+$lang['web_access_control_apply_ip'] = 'اعمال کنترل دسترسی به IP آدرس';
+$lang['web_access_control_apply_mac'] = 'اعمال کنترل دسترسی به مک آدرس';
+$lang['web_access_control_ctrl_click'] = '(CTRL-کلیک کنید برای انتخاب های چندگانه)';
+$lang['web_access_control_day_of_week'] = 'روز هفته';
+$lang['web_access_control_end_time'] = 'زمان پایان';
+$lang['web_access_control_id_method'] = 'روش شناسایی';
+$lang['web_access_control_list'] = 'لیست کنترل دسترسی';
+$lang['web_access_control_name'] = 'نام';
+$lang['web_access_control_outside'] = 'خارج از';
+$lang['web_access_control_outside_range'] = 'محدودیت زمان در خارج';
+$lang['web_access_control_priority'] = 'اولویت';
+$lang['web_access_control_start_time'] = 'زمان شروع';
+$lang['web_access_control_time_definitions'] = 'زمان روز تعاریف';
+$lang['web_access_control_time_delete_warning'] = 'هشدار: هر ورودی کنترل دسترسی استفاده از این تعریف هم حذف خواهد شد.';
+$lang['web_access_control_time_of_day'] = 'زمان روز';
+$lang['web_access_control_time_restriction'] = 'محدودیت';
+$lang['web_access_control_time_rule'] = 'زمان از روز کنترل دسترسی';
+$lang['web_access_control_type'] = 'نوع';
+$lang['web_access_control_web_access_control'] = 'کنترل دسترسی به وب سایت';
+$lang['web_access_control_within'] = 'در داخل';
+$lang['web_access_control_within_range'] = 'در محدودیت زمانی';
