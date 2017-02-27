492cff436d272d31b8fdfa44857ac94bc7d5f1a3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:18:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Add Time Period';
+$lang['web_access_control_app_description'] = 'Web Access Control damar mai gudanarwa a tilasta lokaci-of-rana yanar gizo access to kungiyoyin ko kwakwalwa (IP ko MAC adireshin) ta yin amfani da yanar gizo wakili.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Aiwatar Access Control to Group';
+$lang['web_access_control_apply_ip'] = 'Aiwatar Access Control to IP Address';
+$lang['web_access_control_apply_mac'] = 'Aiwatar Access Control to MAC Address';
+$lang['web_access_control_ctrl_click'] = '(CTRL-click for mahara zabe)';
+$lang['web_access_control_day_of_week'] = 'Rãnar Week';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Hanyar Identification';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'name';
+$lang['web_access_control_outside'] = 'A waje';
+$lang['web_access_control_outside_range'] = 'A waje lokaci ƙuntatawa';
+$lang['web_access_control_priority'] = 'Fifiko';
+$lang['web_access_control_start_time'] = 'Fara Time';
+$lang['web_access_control_time_definitions'] = 'Time of Day Ma&#39;anar';
+$lang['web_access_control_time_delete_warning'] = 'Gargadi: wani hanya iko shigarwar amfani da wannan lokaci definition za a share.';
+$lang['web_access_control_time_of_day'] = 'Time of Day';
+$lang['web_access_control_time_restriction'] = 'ƙuntatawa';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'type';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'a cikin';
+$lang['web_access_control_within_range'] = 'A cikin lokaci ƙuntatawa';
