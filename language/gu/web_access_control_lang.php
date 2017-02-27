4d9e125874868f6b6f44f44a0d115aed03306517
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:17:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'પીરિયડ ઉમેરો';
+$lang['web_access_control_app_description'] = 'વેબ ઍક્સેસ નિયંત્રણ જૂથો અથવા કોમ્પ્યુટર્સ (IP છે અથવા MAC સરનામું) વેબ પ્રોક્સી વાપરી રહ્યા સમય ની દિવસ વેબ વપરાશ લાગુ પાડવા માટે એક વ્યવસ્થાપક પરવાનગી આપે છે.';
+$lang['web_access_control_app_name'] = 'વેબ ઍક્સેસ નિયંત્રણ';
+$lang['web_access_control_apply_group'] = 'ગ્રુપ ઍક્સેસ નિયંત્રણ લાગુ';
+$lang['web_access_control_apply_ip'] = 'IP સરનામું ઍક્સેસ નિયંત્રણ લાગુ';
+$lang['web_access_control_apply_mac'] = 'MAC સરનામું ઍક્સેસ નિયંત્રણ લાગુ';
+$lang['web_access_control_ctrl_click'] = '(ઘણી પસંદગીઓ કરવા માટે CTRL-ક્લિક કરો)';
+$lang['web_access_control_day_of_week'] = 'અઠવાડિયાના દિવસ';
+$lang['web_access_control_end_time'] = 'અંત સમય';
+$lang['web_access_control_id_method'] = 'ઓળખ પદ્ધતિ';
+$lang['web_access_control_list'] = 'ઍક્સેસ નિયંત્રણ સૂચી';
+$lang['web_access_control_name'] = 'નામ';
+$lang['web_access_control_outside'] = 'બહાર';
+$lang['web_access_control_outside_range'] = 'બહાર સમય બંધનો';
+$lang['web_access_control_priority'] = 'પ્રાથમિકતા';
+$lang['web_access_control_start_time'] = 'પ્રારંભ સમય';
+$lang['web_access_control_time_definitions'] = 'ડે વ્યાખ્યાઓ સમય';
+$lang['web_access_control_time_delete_warning'] = 'ચેતવણી: કોઈપણ વપરાશ નિયંત્રણ આ સમય વ્યાખ્યા મદદથી પ્રવેશો કાઢી નાખવામાં આવશે.';
+$lang['web_access_control_time_of_day'] = 'દિવસ ના ભાવ';
+$lang['web_access_control_time_restriction'] = 'નિયંત્રણ';
+$lang['web_access_control_time_rule'] = 'સમય ના-ડે ઍક્સેસ નિયંત્રણ';
+$lang['web_access_control_type'] = 'પ્રકાર';
+$lang['web_access_control_web_access_control'] = 'વેબ ઍક્સેસ નિયંત્રણ';
+$lang['web_access_control_within'] = 'અંદર';
+$lang['web_access_control_within_range'] = 'સમય બંધનો અંદર';
