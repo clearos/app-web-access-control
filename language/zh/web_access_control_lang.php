b644281cc0a327241a99e4b23d8481c55e4d328f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:16:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = '添加时间段';
+$lang['web_access_control_app_description'] = 'Web访问控制，管理员可以强制执行组或使用Web代理计算机（IP或MAC地址）时的一天Web访问。';
+$lang['web_access_control_app_name'] = 'Web访问控制';
+$lang['web_access_control_apply_group'] = '应用访问控制组';
+$lang['web_access_control_apply_ip'] = '应用访问控制的IP地址';
+$lang['web_access_control_apply_mac'] = '应用访问控制MAC地址';
+$lang['web_access_control_ctrl_click'] = '（多选CTRL点击）';
+$lang['web_access_control_day_of_week'] = '星期几';
+$lang['web_access_control_end_time'] = '时间结束';
+$lang['web_access_control_id_method'] = '识别方法';
+$lang['web_access_control_list'] = '访问控制列表';
+$lang['web_access_control_name'] = '名称';
+$lang['web_access_control_outside'] = '外';
+$lang['web_access_control_outside_range'] = '外面的时间限制';
+$lang['web_access_control_priority'] = '优先';
+$lang['web_access_control_start_time'] = '开始时间';
+$lang['web_access_control_time_definitions'] = '天定义的时间';
+$lang['web_access_control_time_delete_warning'] = '警告：使用这个时间定义的任何访问控制项都将被删除。';
+$lang['web_access_control_time_of_day'] = '当日时间';
+$lang['web_access_control_time_restriction'] = '限制';
+$lang['web_access_control_time_rule'] = '时间的日访问控制';
+$lang['web_access_control_type'] = '类型';
+$lang['web_access_control_web_access_control'] = 'Web访问控制';
+$lang['web_access_control_within'] = '中';
+$lang['web_access_control_within_range'] = '在时间限制';
