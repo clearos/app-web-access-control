8e2638d8b04156394871ced52af08a7f8bc20128
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:20:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'ເພີ່ມໄລຍະເວລາທີ່ໃຊ້ເວລາ';
+$lang['web_access_control_app_description'] = 'Web Access Control ອະນຸຍາດໃຫ້ຜູ້ບໍລິຫານການບັງຄັບໃຊ້ທີ່ໃຊ້ເວລາຂອງມື້ເຂົ້າເຖິງເວັບໄຊຕ໌ກຸ່ມຫຼືຄອມພິວເຕີ (IP ຫຼືທີ່ຢູ່ MAC) ການນໍາໃຊ້ຕົວແທນເວັບໄຊຕ໌.';
+$lang['web_access_control_app_name'] = 'ການຄວບຄຸມການເຂົ້າເຖິງເວັບໄຊຕ໌';
+$lang['web_access_control_apply_group'] = 'ສະຫມັກຕໍາຄວບຄຸມການເຂົ້າເຖິງກຸ່ມ';
+$lang['web_access_control_apply_ip'] = 'ສະຫມັກຕໍາຄວບຄຸມການເຂົ້າເຖິງທີ່ຢູ່ IP';
+$lang['web_access_control_apply_mac'] = 'ສະຫມັກຕໍາຄວບຄຸມການເຂົ້າເຖິງທີ່ຢູ່ MAC';
+$lang['web_access_control_ctrl_click'] = '(CTRL ຄລິກເລືອກຫຼາຍ)';
+$lang['web_access_control_day_of_week'] = 'ມື້ຂອງອາທິດ';
+$lang['web_access_control_end_time'] = 'ທີ່ໃຊ້ເວລາສຸດທ້າຍ';
+$lang['web_access_control_id_method'] = 'ວິທີການຈໍາແນກຊະນິດ';
+$lang['web_access_control_list'] = 'ການເຂົ້າເຖິງບັນຊີຄວບຄຸມ';
+$lang['web_access_control_name'] = 'ຊື່';
+$lang['web_access_control_outside'] = 'ນອກ';
+$lang['web_access_control_outside_range'] = 'ຂໍ້ຈໍາກັດທີ່ໃຊ້ເວລານອກ';
+$lang['web_access_control_priority'] = 'ບູລິມະສິດ';
+$lang['web_access_control_start_time'] = 'ທີ່ໃຊ້ເວລາເລີ່ມຕົ້ນ';
+$lang['web_access_control_time_definitions'] = 'ທີ່ໃຊ້ເວລາຂອງມື້ປະ';
+$lang['web_access_control_time_delete_warning'] = 'ການເຕືອນໄພ: ສະ entries ການຄວບຄຸມການເຂົ້າເຖິງການນໍາໃຊ້ຄໍານິຍາມທີ່ໃຊ້ເວລານີ້ຈະໄດ້ຮັບການລຶບ.';
+$lang['web_access_control_time_of_day'] = 'ທີ່ໃຊ້ເວລາຂອງມື້';
+$lang['web_access_control_time_restriction'] = 'ການຈໍາກັດ';
+$lang['web_access_control_time_rule'] = 'ທີ່ໃຊ້ເວລາຂອງມື້ Access Control';
+$lang['web_access_control_type'] = 'ປະເພດ';
+$lang['web_access_control_web_access_control'] = 'ການຄວບຄຸມການເຂົ້າເຖິງເວັບໄຊຕ໌';
+$lang['web_access_control_within'] = 'ພາຍໃນ';
+$lang['web_access_control_within_range'] = 'ພາຍໃນມີຂໍ້ຈໍາກັດທີ່ໃຊ້ເວລາ';
