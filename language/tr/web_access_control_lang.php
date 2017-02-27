ebc08e88bab6f32abfcc5a3c1ba28b4f040e72f4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:24:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Zaman Dönemi ekle';
+$lang['web_access_control_app_description'] = 'Web Erişim Denetimi gruplar veya web proxy kullanarak bilgisayarlar (IP veya MAC adresi) zaman-of-day web erişimi zorlamak için bir yönetici sağlar.';
+$lang['web_access_control_app_name'] = 'Web Erişim Denetimi';
+$lang['web_access_control_apply_group'] = 'Grup Erişim Kontrolü uygulayın';
+$lang['web_access_control_apply_ip'] = 'IP Adresi Erişim Kontrolü uygulayın';
+$lang['web_access_control_apply_mac'] = 'MAC Adresi Erişim Kontrolü uygulayın';
+$lang['web_access_control_ctrl_click'] = '(Çoklu seçim için CTRL tıklama)';
+$lang['web_access_control_day_of_week'] = 'Haftanın günü';
+$lang['web_access_control_end_time'] = 'Bitiş zamanı';
+$lang['web_access_control_id_method'] = 'Kimlik Yöntemi';
+$lang['web_access_control_list'] = 'Erişim Kontrol Listesi';
+$lang['web_access_control_name'] = 'isim';
+$lang['web_access_control_outside'] = 'Dışında';
+$lang['web_access_control_outside_range'] = 'Dış zaman kısıtlamaları';
+$lang['web_access_control_priority'] = 'öncelik';
+$lang['web_access_control_start_time'] = 'Başlama zamanı';
+$lang['web_access_control_time_definitions'] = 'Gün Tanımlar Zamanı';
+$lang['web_access_control_time_delete_warning'] = 'Uyarı: Bu sefer tanımı kullanarak herhangi bir erişim denetim girdileri silinecektir.';
+$lang['web_access_control_time_of_day'] = 'Günün zamanı';
+$lang['web_access_control_time_restriction'] = 'kısıtlama';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Erişim Kontrolü';
+$lang['web_access_control_type'] = 'tip';
+$lang['web_access_control_web_access_control'] = 'Web Erişim Denetimi';
+$lang['web_access_control_within'] = 'içinde';
+$lang['web_access_control_within_range'] = 'zaman kısıtlamaları içinde';
