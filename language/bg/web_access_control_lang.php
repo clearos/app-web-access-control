7f00144b0e0c74481134e9bbe8b14867f790f8af
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:15:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Добави Време Период';
+$lang['web_access_control_app_description'] = 'Уеб контрол на достъпа позволява на администратора да наложат време на деня достъп до мрежата за групи или компютри (IP или MAC адрес), с помощта на уеб прокси.';
+$lang['web_access_control_app_name'] = 'Контрол Web Access';
+$lang['web_access_control_apply_group'] = 'Нанесете за контрол на достъпа до група';
+$lang['web_access_control_apply_ip'] = 'Нанесете за контрол на достъпа до IP адрес';
+$lang['web_access_control_apply_mac'] = 'Нанесете за контрол на достъпа до MAC адрес';
+$lang['web_access_control_ctrl_click'] = '(CTRL-кликнете за няколко селекции)';
+$lang['web_access_control_day_of_week'] = 'Ден на седмицата';
+$lang['web_access_control_end_time'] = 'Крайно време';
+$lang['web_access_control_id_method'] = 'Начин на идентификация';
+$lang['web_access_control_list'] = 'Списък за контрол на достъпа';
+$lang['web_access_control_name'] = 'име';
+$lang['web_access_control_outside'] = 'извън';
+$lang['web_access_control_outside_range'] = 'Външни времеви ограничения';
+$lang['web_access_control_priority'] = 'приоритет';
+$lang['web_access_control_start_time'] = 'Начален час';
+$lang['web_access_control_time_definitions'] = 'Време на деня Определения';
+$lang['web_access_control_time_delete_warning'] = 'Внимание: всички записи за контрол на достъпа с използване на това определение време ще бъдат изтрити.';
+$lang['web_access_control_time_of_day'] = 'Време на деня';
+$lang['web_access_control_time_restriction'] = 'ограничение';
+$lang['web_access_control_time_rule'] = 'Време на деня за контрол на достъпа';
+$lang['web_access_control_type'] = 'Тип';
+$lang['web_access_control_web_access_control'] = 'Контрол Web Access';
+$lang['web_access_control_within'] = 'В рамките на';
+$lang['web_access_control_within_range'] = 'В рамките на ограниченията на времето';
