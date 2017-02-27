8026961a4e7a57fcdf646188a016cc969e940421
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:16:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Dodaj vremenskom razdoblju';
+$lang['web_access_control_app_description'] = 'Web Access Control omogućuje administratoru za provođenje vremena na dan web pristup grupama ili računala (IP ili MAC adresa) Korištenje web proxy.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Za kontrolu pristupa u grupu';
+$lang['web_access_control_apply_ip'] = 'Za kontrolu pristupa na IP adresa';
+$lang['web_access_control_apply_mac'] = 'Nanesite kontrolu pristupa MAC adresa';
+$lang['web_access_control_ctrl_click'] = '(Ctrl-klik na višestruki odabir)';
+$lang['web_access_control_day_of_week'] = 'Dan u tjednu';
+$lang['web_access_control_end_time'] = 'Vrijeme kraja';
+$lang['web_access_control_id_method'] = 'Postupak identifikacije';
+$lang['web_access_control_list'] = 'Popis postupaka nadzora pristupa';
+$lang['web_access_control_name'] = 'Ime';
+$lang['web_access_control_outside'] = 'izvan';
+$lang['web_access_control_outside_range'] = 'Izvan vremenskih ograničenja';
+$lang['web_access_control_priority'] = 'Prioritet';
+$lang['web_access_control_start_time'] = 'Vrijeme početka';
+$lang['web_access_control_time_definitions'] = 'Doba dana Definicije';
+$lang['web_access_control_time_delete_warning'] = 'Upozorenje: sve kontrole pristupa unosi koriste ovaj put definicija će biti izbrisani.';
+$lang['web_access_control_time_of_day'] = 'Vrijeme dana';
+$lang['web_access_control_time_restriction'] = 'Ograničenje';
+$lang['web_access_control_time_rule'] = 'Time-of-day kontrolu pristupa';
+$lang['web_access_control_type'] = 'Tip';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Unutar';
+$lang['web_access_control_within_range'] = 'Unutar vremenskog ograničenja';
