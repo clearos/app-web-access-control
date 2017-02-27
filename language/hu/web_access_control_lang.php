603636a4ff40481c20268954fb9ddffd84f8a656
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:18:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Add Időszak';
+$lang['web_access_control_app_description'] = 'Web Access Control lehetővé teszi az adminisztrátor érvényesíteni idő-of-day internetes hozzáférést a csoportok vagy számítógép (IP vagy MAC cím) felhasználásával a web proxy.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Hozzáférés-vezérlő és a Csoport';
+$lang['web_access_control_apply_ip'] = 'Hozzáférés-vezérlő az IP-cím';
+$lang['web_access_control_apply_mac'] = 'Hozzáférés-vezérlő, hogy a MAC-cím';
+$lang['web_access_control_ctrl_click'] = '(CTRL + egér gomb többszörös kijelölés)';
+$lang['web_access_control_day_of_week'] = 'A hét napja';
+$lang['web_access_control_end_time'] = 'Idő vége';
+$lang['web_access_control_id_method'] = 'Az azonosítás módja';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'Név';
+$lang['web_access_control_outside'] = 'Kívül';
+$lang['web_access_control_outside_range'] = 'Kívül idő korlátozások';
+$lang['web_access_control_priority'] = 'Kiemelten fontos';
+$lang['web_access_control_start_time'] = 'Kezdési idő';
+$lang['web_access_control_time_definitions'] = 'Napi idő meghatározások';
+$lang['web_access_control_time_delete_warning'] = 'Figyelmeztetés: bármely beléptető bejegyzéseket ezúttal meghatározás törlésre kerül.';
+$lang['web_access_control_time_of_day'] = 'Napszak';
+$lang['web_access_control_time_restriction'] = 'Korlátozás';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'típus';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Belül';
+$lang['web_access_control_within_range'] = 'Időn belül korlátozás';
