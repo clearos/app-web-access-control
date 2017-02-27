13bf959f25b21457614d65fa9be4e9d7f179e023
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:19:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'ಕಾಲಾವಧಿ ಸೇರಿಸಿ';
+$lang['web_access_control_app_description'] = 'ವೆಬ್ ಪ್ರವೇಶ ನಿಯಂತ್ರಣ ಗುಂಪುಗಳು ಅಥವಾ ವೆಬ್ ಪ್ರಾಕ್ಸಿ ಅನ್ನು ಕಂಪ್ಯೂಟರ್ಗಳ (ಐಪಿ ಅಥವಾ MAC ವಿಳಾಸ) ಸಮಯ ಯಾ ದಿನ ವೆಬ್ ಪ್ರವೇಶ ಜಾರಿಗೊಳಿಸಲು ನಿರ್ವಾಹಕರಾಗಿ ಅನುಮತಿಸುತ್ತದೆ.';
+$lang['web_access_control_app_name'] = 'ವೆಬ್ ಪ್ರವೇಶ ನಿಯಂತ್ರಣ';
+$lang['web_access_control_apply_group'] = 'ಗ್ರೂಪ್ ಪ್ರವೇಶ ನಿಯಂತ್ರಣ ಅನ್ವಯಿಸು';
+$lang['web_access_control_apply_ip'] = 'IP ವಿಳಾಸ ಗೆ ಪ್ರವೇಶ ಕಂಟ್ರೋಲ್ ಅನ್ವಯಿಸು';
+$lang['web_access_control_apply_mac'] = 'MAC ವಿಳಾಸ ಗೆ ಪ್ರವೇಶ ಕಂಟ್ರೋಲ್ ಅನ್ವಯಿಸು';
+$lang['web_access_control_ctrl_click'] = '(ಅನೇಕ ಆಯ್ಕೆಗಳನ್ನು CTRL-ಕ್ಲಿಕ್)';
+$lang['web_access_control_day_of_week'] = 'ವಾರದ ದಿನ';
+$lang['web_access_control_end_time'] = 'ಎಂಡ್ ಟೈಮ್';
+$lang['web_access_control_id_method'] = 'ಗುರುತಿನ ವಿಧಾನ';
+$lang['web_access_control_list'] = 'ಪ್ರವೇಶ ನಿಯಂತ್ರಣ ಪಟ್ಟಿ';
+$lang['web_access_control_name'] = 'ಹೆಸರು';
+$lang['web_access_control_outside'] = 'ಹೊರಗೆ';
+$lang['web_access_control_outside_range'] = 'ಹೊರಗೆ ಸಮಯ ನಿರ್ಬಂಧಗಳನ್ನು';
+$lang['web_access_control_priority'] = 'ಆದ್ಯತೆ';
+$lang['web_access_control_start_time'] = 'ಆರಂಭವಾಗುವ';
+$lang['web_access_control_time_definitions'] = 'ಡೇ ವ್ಯಾಖ್ಯಾನಗಳು ಟೈಮ್';
+$lang['web_access_control_time_delete_warning'] = 'ಎಚ್ಚರಿಕೆ: ಈ ಬಾರಿ ವ್ಯಾಖ್ಯಾನ ಬಳಸಿ ಯಾವುದೇ ಪ್ರವೇಶ ನಿಯಂತ್ರಣ ನಮೂದುಗಳನ್ನು ಅಳಿಸಲಾಗುತ್ತದೆ.';
+$lang['web_access_control_time_of_day'] = 'ದಿನದ ಸಮಯ';
+$lang['web_access_control_time_restriction'] = 'ನಿರ್ಬಂಧ';
+$lang['web_access_control_time_rule'] = 'ಟೈಮ್-ಆಫ್-ಡೇ ಪ್ರವೇಶ ನಿಯಂತ್ರಣ';
+$lang['web_access_control_type'] = 'ಕೌಟುಂಬಿಕತೆ';
+$lang['web_access_control_web_access_control'] = 'ವೆಬ್ ಪ್ರವೇಶ ನಿಯಂತ್ರಣ';
+$lang['web_access_control_within'] = 'ಒಳಗೆ';
+$lang['web_access_control_within_range'] = 'ಸಮಯ ನಿರ್ಬಂಧಗಳನ್ನು ಒಳಗೆ';
