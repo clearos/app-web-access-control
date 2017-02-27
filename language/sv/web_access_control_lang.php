91d9c4afdfb019c88503ef9d4b09529eee66769e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:24:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Lägg Period';
+$lang['web_access_control_app_description'] = 'Web Access Control kan en administratör att genomdriva time-of-day webbåtkomst till grupper eller datorer (IP eller MAC-adress) med hjälp av webbproxy.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Applicera åtkomstkontroll till grupp';
+$lang['web_access_control_apply_ip'] = 'Applicera åtkomstkontroll till IP-adress';
+$lang['web_access_control_apply_mac'] = 'Applicera åtkomstkontroll till MAC-adress';
+$lang['web_access_control_ctrl_click'] = '(CTRL-klicka för flera val)';
+$lang['web_access_control_day_of_week'] = 'Dag i veckan';
+$lang['web_access_control_end_time'] = 'Sluttid';
+$lang['web_access_control_id_method'] = 'Metod för Identifiering';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'namn';
+$lang['web_access_control_outside'] = 'Utanför';
+$lang['web_access_control_outside_range'] = 'Utanför tidsbegränsningar';
+$lang['web_access_control_priority'] = 'Prioritet';
+$lang['web_access_control_start_time'] = 'Starttid';
+$lang['web_access_control_time_definitions'] = 'Tid på dygnet Definitioner';
+$lang['web_access_control_time_delete_warning'] = 'Varning: alla åtkomstkontroll poster som använder den här gången definition kommer att tas bort.';
+$lang['web_access_control_time_of_day'] = 'Tidpunkt på dygnet';
+$lang['web_access_control_time_restriction'] = 'Begränsning';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'Typ';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Inom';
+$lang['web_access_control_within_range'] = 'Inom tidsbegränsningar';
