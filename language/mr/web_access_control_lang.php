35213760c6662b45e70794a55ac0e6c1c07c20b1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:21:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'कालावधी जोडा';
+$lang['web_access_control_app_description'] = 'वेब प्रवेश नियंत्रण गट किंवा संगणक (आंतरजाल किंवा MAC अॅड्रेस) वेब प्रॉक्सी वापरून वेळ-ऑफ-दिवस वेब प्रवेश अंमलबजावणी प्रशासक परवानगी देते.';
+$lang['web_access_control_app_name'] = 'वेब प्रवेश नियंत्रण';
+$lang['web_access_control_apply_group'] = 'गट प्रवेश नियंत्रण लागू करा';
+$lang['web_access_control_apply_ip'] = 'IP पत्ता एक्सेस कंट्रोल लागू करा';
+$lang['web_access_control_apply_mac'] = 'MAC पत्ता प्रवेश नियंत्रण लागू करा';
+$lang['web_access_control_ctrl_click'] = '(अनेक निवडी करण्यासाठी Ctrl-क्लिक करा)';
+$lang['web_access_control_day_of_week'] = 'आठवड्याचे दिवस';
+$lang['web_access_control_end_time'] = 'समाप्तीचा कालावधी';
+$lang['web_access_control_id_method'] = 'ओळख पद्धत';
+$lang['web_access_control_list'] = 'नियंत्रण यादी प्रवेश';
+$lang['web_access_control_name'] = 'नाव';
+$lang['web_access_control_outside'] = 'बाहेर';
+$lang['web_access_control_outside_range'] = 'बाहेर वेळ निर्बंध';
+$lang['web_access_control_priority'] = 'प्राधान्य';
+$lang['web_access_control_start_time'] = 'सुरवातीची वेळ';
+$lang['web_access_control_time_definitions'] = 'दिवस व्याख्या वेळ';
+$lang['web_access_control_time_delete_warning'] = 'चेतावणी: या वेळी व्याख्या वापरून कोणत्याही प्रवेश कंट्रोल नोंदणी हटवले जाईल.';
+$lang['web_access_control_time_of_day'] = 'दिवसाची वेळ';
+$lang['web_access_control_time_restriction'] = 'निर्बंध';
+$lang['web_access_control_time_rule'] = 'वेळ-ऑफ-दिवस एक्सेस कंट्रोल';
+$lang['web_access_control_type'] = 'प्रकार';
+$lang['web_access_control_web_access_control'] = 'वेब प्रवेश नियंत्रण';
+$lang['web_access_control_within'] = 'आत';
+$lang['web_access_control_within_range'] = 'वेळ निर्बंध आत';
