046238e49acae1a1b2183edc0801b9078efae488
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:22:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Perioada de timp se adaugă';
+$lang['web_access_control_app_description'] = 'Access Control Web permite unui administrator să pună în aplicare timp de zi de acces web la grupuri sau calculatoare (IP sau adresa MAC) utilizând proxy-ul web.';
+$lang['web_access_control_app_name'] = 'Control acces web';
+$lang['web_access_control_apply_group'] = 'Se aplică de control al accesului la Grup';
+$lang['web_access_control_apply_ip'] = 'Controlul accesului se aplică la adresa IP';
+$lang['web_access_control_apply_mac'] = 'Controlul accesului se aplică la MAC Address';
+$lang['web_access_control_ctrl_click'] = '(CTRL plus clic pentru mai multe selecții)';
+$lang['web_access_control_day_of_week'] = 'Zi a săptămânii';
+$lang['web_access_control_end_time'] = 'Ora de terminare';
+$lang['web_access_control_id_method'] = 'Metoda de identificare';
+$lang['web_access_control_list'] = 'Listă de control acces';
+$lang['web_access_control_name'] = 'Nume';
+$lang['web_access_control_outside'] = 'In afara';
+$lang['web_access_control_outside_range'] = 'Restricții în afara timpului';
+$lang['web_access_control_priority'] = 'Prioritate';
+$lang['web_access_control_start_time'] = 'Timpul de începere';
+$lang['web_access_control_time_definitions'] = 'Ora zilei Definiții';
+$lang['web_access_control_time_delete_warning'] = 'Avertizare: intrările de control al accesului utilizând această definiție dată vor fi șterse.';
+$lang['web_access_control_time_of_day'] = 'Ora din zi';
+$lang['web_access_control_time_restriction'] = 'Restricţie';
+$lang['web_access_control_time_rule'] = 'Time-of-day de control al accesului';
+$lang['web_access_control_type'] = 'Tip';
+$lang['web_access_control_web_access_control'] = 'Control acces web';
+$lang['web_access_control_within'] = 'În';
+$lang['web_access_control_within_range'] = 'În termen de restricții temporale';
