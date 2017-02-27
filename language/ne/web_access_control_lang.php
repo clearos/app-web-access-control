315259252ced97093107d6757fb28caa6d152726
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:21:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'समय अवधि थप';
+$lang['web_access_control_app_description'] = 'वेब पहुँच नियन्त्रण समूह वा कम्प्यूटर (आईपी वा म्याक ठेगाना) वेब प्रोक्सी प्रयोग गरेर समय-को-दिन वेब पहुँच लागू गर्न प्रशासक अनुमति दिन्छ।';
+$lang['web_access_control_app_name'] = 'वेब पहुँच नियन्त्रण';
+$lang['web_access_control_apply_group'] = 'समूह पहुँच नियन्त्रण लागू';
+$lang['web_access_control_apply_ip'] = 'आईपी ​​ठेगाना पहुँच नियन्त्रण लागू';
+$lang['web_access_control_apply_mac'] = 'MAC ठेगाना पहुँच नियन्त्रण लागू';
+$lang['web_access_control_ctrl_click'] = '(बहुचयनहरूको लागि Ctrl-क्लिक गर्नुहोस्)';
+$lang['web_access_control_day_of_week'] = 'हप्तामादिन';
+$lang['web_access_control_end_time'] = 'अन्त समय';
+$lang['web_access_control_id_method'] = 'पहिचान को विधि';
+$lang['web_access_control_list'] = 'पहुँच नियन्त्रण सूची';
+$lang['web_access_control_name'] = 'नाम';
+$lang['web_access_control_outside'] = 'बाहिर';
+$lang['web_access_control_outside_range'] = 'बाहिर समय प्रतिबन्ध';
+$lang['web_access_control_priority'] = 'प्राथमिकता';
+$lang['web_access_control_start_time'] = 'समय सुरु';
+$lang['web_access_control_time_definitions'] = 'दिन परिभाषा को समय';
+$lang['web_access_control_time_delete_warning'] = 'चेतावनी: यो समय परिभाषा प्रयोग कुनै पनि पहुँच नियन्त्रण प्रविष्टिहरू मेटिनेछ।';
+$lang['web_access_control_time_of_day'] = 'दिन को समय';
+$lang['web_access_control_time_restriction'] = 'प्रतिबन्ध';
+$lang['web_access_control_time_rule'] = 'समय-को-दिन पहुँच नियन्त्रण';
+$lang['web_access_control_type'] = 'प्रकार';
+$lang['web_access_control_web_access_control'] = 'वेब पहुँच नियन्त्रण';
+$lang['web_access_control_within'] = 'भित्र';
+$lang['web_access_control_within_range'] = 'समय प्रतिबन्ध भित्र';
