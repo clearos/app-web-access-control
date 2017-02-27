b36d97126643bef3fc958365be5628cc559cacc8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:22:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Dodaj okresie czasu';
+$lang['web_access_control_app_description'] = 'Kontrola dostępu do sieci pozwala administratorowi wymusić czasu rzeczywistego dzień dostępu do sieci do grup lub komputerów (IP lub adres MAC) z wykorzystaniem serwera proxy WWW.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Zastosuj do grupy Access Control';
+$lang['web_access_control_apply_ip'] = 'Zastosuj kontrolować dostęp do adresu IP';
+$lang['web_access_control_apply_mac'] = 'Zastosuj kontrolować dostęp do adresu MAC';
+$lang['web_access_control_ctrl_click'] = '(CTRL-click for wielokrotnego wyboru)';
+$lang['web_access_control_day_of_week'] = 'Dzień tygodnia';
+$lang['web_access_control_end_time'] = 'Koniec czasu';
+$lang['web_access_control_id_method'] = 'Metoda identyfikacji';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'Nazwa';
+$lang['web_access_control_outside'] = 'Na zewnątrz';
+$lang['web_access_control_outside_range'] = 'ograniczenia czasu poza';
+$lang['web_access_control_priority'] = 'Priorytet';
+$lang['web_access_control_start_time'] = 'Czas rozpoczęcia';
+$lang['web_access_control_time_definitions'] = 'Pora dnia Definicje';
+$lang['web_access_control_time_delete_warning'] = 'Uwaga: wszelkie wpisy kontroli dostępu z wykorzystaniem tej definicji czasu zostaną usunięte.';
+$lang['web_access_control_time_of_day'] = 'Pora dnia';
+$lang['web_access_control_time_restriction'] = 'Ograniczenie';
+$lang['web_access_control_time_rule'] = 'Time-of-day Access Control';
+$lang['web_access_control_type'] = 'Rodzaj';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'W ciągu';
+$lang['web_access_control_within_range'] = 'W ramach ograniczeń czasowych';
