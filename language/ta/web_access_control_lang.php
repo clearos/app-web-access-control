16dc765111362b7cb161afb1667db16f962bab87
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:24:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'நேரம் காலம் சேர்';
+$lang['web_access_control_app_description'] = 'வலை அணுகல் கட்டுப்பாடு குழுக்கள் அல்லது கணினிகள் (ஐபி அல்லது MAC முகவரி) வலை பதிலி பயன்படுத்தி நேரம் மற்றும் நாளின் இணைய செயல்படுத்த ஒரு நிர்வாகி அனுமதிக்கிறது.';
+$lang['web_access_control_app_name'] = 'வலை அணுகல் கட்டுப்பாடு';
+$lang['web_access_control_apply_group'] = 'குழு அணுகல் கட்டுப்பாடு விண்ணப்பிக்கவும்';
+$lang['web_access_control_apply_ip'] = 'ஐபி முகவரி அணுகல் கட்டுப்பாடு விண்ணப்பிக்கவும்';
+$lang['web_access_control_apply_mac'] = 'MAC முகவரி அணுகல் கட்டுப்பாடு விண்ணப்பிக்கவும்';
+$lang['web_access_control_ctrl_click'] = '(பல தேர்வுகளை க்கான CTRL கிளிக்)';
+$lang['web_access_control_day_of_week'] = 'வார நாள்';
+$lang['web_access_control_end_time'] = 'முடிவு நேரம்';
+$lang['web_access_control_id_method'] = 'அடையாள முறை';
+$lang['web_access_control_list'] = 'அணுகல் கட்டுப்பாடு பட்டியல்';
+$lang['web_access_control_name'] = 'பெயர்';
+$lang['web_access_control_outside'] = 'வெளியே';
+$lang['web_access_control_outside_range'] = 'வெளியே நேரம் கட்டுப்பாடுகள்';
+$lang['web_access_control_priority'] = 'முன்னுரிமை';
+$lang['web_access_control_start_time'] = 'ஆரம்பிக்கும் நேரம்';
+$lang['web_access_control_time_definitions'] = 'தினம் வரையறைகள் நேரம்';
+$lang['web_access_control_time_delete_warning'] = 'எச்சரிக்கை: இந்த நேரத்தில் வரையறை பயன்படுத்தி எந்த அணுகல் கட்டுப்பாடு உள்ளீடுகளை நீக்கப்படும்.';
+$lang['web_access_control_time_of_day'] = 'நாள் நேரம்';
+$lang['web_access_control_time_restriction'] = 'கட்டுப்பாடு';
+$lang['web_access_control_time_rule'] = 'நேரம், நாள் அணுகல் கட்டுப்பாடு';
+$lang['web_access_control_type'] = 'வகை';
+$lang['web_access_control_web_access_control'] = 'வலை அணுகல் கட்டுப்பாடு';
+$lang['web_access_control_within'] = 'உள்ள';
+$lang['web_access_control_within_range'] = 'நேரம் கட்டுப்பாடுகளுக்கு அமைய';
