ad720035260536e8d867cf6c8fc87b314751d0d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:22:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Додај Тиме Период';
+$lang['web_access_control_app_description'] = 'Веб Аццесс Цонтрол омогућава администратору да спроведе време-оф-даи веб приступ групама или компјутера (ИП или МАЦ адреса) користећи веб проки.';
+$lang['web_access_control_app_name'] = 'Веб Аццесс Цонтрол';
+$lang['web_access_control_apply_group'] = 'Нанесите контролу приступа у групу';
+$lang['web_access_control_apply_ip'] = 'Нанесите контролу приступа на ИП адресе';
+$lang['web_access_control_apply_mac'] = 'Нанесите контролу приступа МАЦ Аддресс';
+$lang['web_access_control_ctrl_click'] = '(Цтрл-клик за више опција)';
+$lang['web_access_control_day_of_week'] = 'Дан у недељи';
+$lang['web_access_control_end_time'] = 'Време завршетка';
+$lang['web_access_control_id_method'] = 'Метода идентификације';
+$lang['web_access_control_list'] = 'Аццесс Цонтрол Лист';
+$lang['web_access_control_name'] = 'име';
+$lang['web_access_control_outside'] = 'ван';
+$lang['web_access_control_outside_range'] = 'Изван времена ограничења';
+$lang['web_access_control_priority'] = 'приоритет';
+$lang['web_access_control_start_time'] = 'Почетно време';
+$lang['web_access_control_time_definitions'] = 'Доба дана Дефиниције';
+$lang['web_access_control_time_delete_warning'] = 'Упозорење: сви уноси контролу приступа који користе овај пут дефиницију ће бити избрисани.';
+$lang['web_access_control_time_of_day'] = 'Доба дана';
+$lang['web_access_control_time_restriction'] = 'ограничење';
+$lang['web_access_control_time_rule'] = 'Време-на-Даи Аццесс Цонтрол';
+$lang['web_access_control_type'] = 'Тип';
+$lang['web_access_control_web_access_control'] = 'Веб Аццесс Цонтрол';
+$lang['web_access_control_within'] = 'У склопу';
+$lang['web_access_control_within_range'] = 'Унутар временских ограничења';
