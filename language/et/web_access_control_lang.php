486614ed01379a04c707b9d003aaf92c010e06fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:17:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Lisa Ajaperioodi';
+$lang['web_access_control_app_description'] = 'Web Access Control võimaldab administraatori jõustada aeg-of-päevase veebis rühmadele või arvuteid (IP või MAC aadress), kasutades web proxy.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Kanna Access Control Group';
+$lang['web_access_control_apply_ip'] = 'Kanna Access Control IP aadress';
+$lang['web_access_control_apply_mac'] = 'Kanna Access Control MAC aadress';
+$lang['web_access_control_ctrl_click'] = '(CTRL nuppu, kui mitu valikut)';
+$lang['web_access_control_day_of_week'] = 'Nädalapäev';
+$lang['web_access_control_end_time'] = 'Lõpuaeg';
+$lang['web_access_control_id_method'] = 'Identifitseerimisviis';
+$lang['web_access_control_list'] = 'Pääsuloendi';
+$lang['web_access_control_name'] = 'nimi';
+$lang['web_access_control_outside'] = 'väljaspool';
+$lang['web_access_control_outside_range'] = 'Väljaspool aega piirangud';
+$lang['web_access_control_priority'] = 'Prioriteet';
+$lang['web_access_control_start_time'] = 'Algusaeg';
+$lang['web_access_control_time_definitions'] = 'Kellaaeg Mõisted';
+$lang['web_access_control_time_delete_warning'] = 'Hoiatus: ükskõik juurdepääsu kontrolli kasutavad kirjed seekord määratlus kustutatakse.';
+$lang['web_access_control_time_of_day'] = 'Kellaaeg';
+$lang['web_access_control_time_restriction'] = 'piirang';
+$lang['web_access_control_time_rule'] = 'Aeg-of-Day Access Control';
+$lang['web_access_control_type'] = 'tüüp';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'jooksul';
+$lang['web_access_control_within_range'] = 'Aja jooksul piirangud';
