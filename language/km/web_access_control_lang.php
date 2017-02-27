cdfb21287f088e00346c3a5073a6b344dc2dab08
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:19:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'បន្ថែមពេលវេលា';
+$lang['web_access_control_app_description'] = 'ត្រួតពិនិត្យការចូលដំណើរការដែលអនុញ្ញាតឱ្យអ្នកគ្រប់គ្រងបណ្ដាញដើម្បីពង្រឹងការចូលដំណើរការបណ្ដាញពេលនៃថ្ងៃដល់ក្រុមឬកុំព្យូទ័រ (IP ឬអាសយដ្ឋាន MAC) ដោយប្រើប្រូកស៊ីបណ្តាញមួយ។';
+$lang['web_access_control_app_name'] = 'ត្រួតពិនិត្យដំណើរការបណ្តាញ';
+$lang['web_access_control_apply_group'] = 'ការត្រួតពិនិត្យការចូលដំណើរការទៅអនុវត្តជាក្រុម';
+$lang['web_access_control_apply_ip'] = 'អនុវត្តការត្រួតពិនិត្យការចូលដំណើរការទៅកាន់អាសយដ្ឋាន IP';
+$lang['web_access_control_apply_mac'] = 'អនុវត្តការត្រួតពិនិត្យការចូលដំណើរការទៅអាសយដ្ឋាន MAC';
+$lang['web_access_control_ctrl_click'] = '(Ctrl-ចុចជ្រើសរើសច្រើន)';
+$lang['web_access_control_day_of_week'] = 'ថ្ងៃនៃសប្តាហ៍';
+$lang['web_access_control_end_time'] = 'ពេលវេលាបញ្ចប់';
+$lang['web_access_control_id_method'] = 'វិធីសាស្រ្តនៃការកំណត់អត្តសញ្ញាណ';
+$lang['web_access_control_list'] = 'បញ្ជីត្រួតពិនិត្យការចូលដំណើរការ';
+$lang['web_access_control_name'] = 'ឈ្មោះ';
+$lang['web_access_control_outside'] = 'នៅខាងក្រៅ';
+$lang['web_access_control_outside_range'] = 'ការរឹតបន្តឹងពេលខាងក្រៅ';
+$lang['web_access_control_priority'] = 'អាទិភាព';
+$lang['web_access_control_start_time'] = 'ចាប់ផ្តើមម៉ោង';
+$lang['web_access_control_time_definitions'] = 'ពេលវេលានៃថ្ងៃនិយមន័យ';
+$lang['web_access_control_time_delete_warning'] = 'ព្រមាន & ‧;: ធាតុត្រួតត្រាការចូលដំណើរការណាមួយដោយប្រើនិយមន័យពេលនេះនឹងត្រូវបានលុប។';
+$lang['web_access_control_time_of_day'] = 'ពេលវេលា​នៃ​ថ្ងៃ';
+$lang['web_access_control_time_restriction'] = 'ការដាក់កម្រិត';
+$lang['web_access_control_time_rule'] = 'ពេលវេលានៃការត្រួតពិនិត្យការចូលដំណើរថ្ងៃ';
+$lang['web_access_control_type'] = 'ប្រភេទ';
+$lang['web_access_control_web_access_control'] = 'ត្រួតពិនិត្យដំណើរការបណ្តាញ';
+$lang['web_access_control_within'] = 'នៅក្នុង';
+$lang['web_access_control_within_range'] = 'នៅក្នុងការរឹតបន្តឹងពេល';
