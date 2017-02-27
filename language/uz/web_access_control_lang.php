bd48c031f78a00eb5eacf6a21076456b26bc3e50
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:25:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Vaqt davri qo&#39;shish';
+$lang['web_access_control_app_description'] = 'Web Access Control guruhlar yoki veb-proksi foydalanish kompyuter (IP yoki MAC-manzil) vaqti-of-kuni Internetga majburlash uchun administrator beradi.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Guruhiga Access Control Qo&#39;llash';
+$lang['web_access_control_apply_ip'] = 'IP-manzil Access Control Qo&#39;llash';
+$lang['web_access_control_apply_mac'] = 'MAC Manzil Access Control Qo&#39;llash';
+$lang['web_access_control_ctrl_click'] = '(Bir necha saylovlari uchun CTRL-bosing)';
+$lang['web_access_control_day_of_week'] = 'Haftaligi kuni';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Tanib olish usuli';
+$lang['web_access_control_list'] = 'Erkin foydalanish nazorat ro&#39;yxati';
+$lang['web_access_control_name'] = 'ism';
+$lang['web_access_control_outside'] = 'tashqarida';
+$lang['web_access_control_outside_range'] = 'Ko&#39;cha vaqt cheklashlar';
+$lang['web_access_control_priority'] = 'ustuvor';
+$lang['web_access_control_start_time'] = 'Boshlanish vaqti';
+$lang['web_access_control_time_definitions'] = 'Kun belgilaydi Time';
+$lang['web_access_control_time_delete_warning'] = 'Ogohlantirish: Ushbu vaqt ta&#39;rifi yordamida har qanday erkin foydalanish nazorat yozuvlari o&#39;chiriladi.';
+$lang['web_access_control_time_of_day'] = 'Kun vaqti';
+$lang['web_access_control_time_restriction'] = 'cheklash';
+$lang['web_access_control_time_rule'] = 'Time-of-kuni Access nazorat';
+$lang['web_access_control_type'] = 'shrift';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'ichida';
+$lang['web_access_control_within_range'] = 'vaqt cheklashlar doirasida';
