e28224aa1aeeee03afc4c0b424e69d3c645e6fb7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:16:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Přidat časové periody';
+$lang['web_access_control_app_description'] = 'Control Web Access umožňuje správci vynutit time-of-denní webový přístup ke skupinám nebo počítačů (IP nebo MAC adresy) za použití web proxy.';
+$lang['web_access_control_app_name'] = 'Control Web Access';
+$lang['web_access_control_apply_group'] = 'Použít řízení přístupu do skupiny';
+$lang['web_access_control_apply_ip'] = 'Použít řízení přístupu na IP adresy';
+$lang['web_access_control_apply_mac'] = 'Použít řízení přístupu MAC Address';
+$lang['web_access_control_ctrl_click'] = '(CTRL-klikněte pro více výběrů)';
+$lang['web_access_control_day_of_week'] = 'Den v týdnu';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Způsob identifikace';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'název';
+$lang['web_access_control_outside'] = 'Mimo';
+$lang['web_access_control_outside_range'] = 'Mimo časová omezení';
+$lang['web_access_control_priority'] = 'Přednost';
+$lang['web_access_control_start_time'] = 'Doba spuštění';
+$lang['web_access_control_time_definitions'] = 'Denní doby Definice';
+$lang['web_access_control_time_delete_warning'] = 'Upozornění: všechny položky řízení přístupu používající tuto definici času budou vymazány.';
+$lang['web_access_control_time_of_day'] = 'Denní doba';
+$lang['web_access_control_time_restriction'] = 'Omezení';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'Typ';
+$lang['web_access_control_web_access_control'] = 'Control Web Access';
+$lang['web_access_control_within'] = 'V rámci';
+$lang['web_access_control_within_range'] = 'V rámci časových omezení';
