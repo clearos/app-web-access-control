dfab1efde8c6fc647d89b54f908bf58ad84adc9a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:25:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Додати Період часу';
+$lang['web_access_control_app_description'] = 'Веб Контроль доступу дозволяє адміністратору забезпечувати дотримання часу в день веб-доступ до груп або комп&#39;ютерів (IP або MAC-адресу), використовуючи веб-проксі.';
+$lang['web_access_control_app_name'] = 'Контроль Web Access';
+$lang['web_access_control_apply_group'] = 'Застосувати Управління доступом до групи';
+$lang['web_access_control_apply_ip'] = 'Застосувати контроль доступу до IP-адреса';
+$lang['web_access_control_apply_mac'] = 'Застосувати контроль доступу до MAC-адресами';
+$lang['web_access_control_ctrl_click'] = '(CTRL-клік для вибору кількох)';
+$lang['web_access_control_day_of_week'] = 'день тижня';
+$lang['web_access_control_end_time'] = 'час закінчення';
+$lang['web_access_control_id_method'] = 'метод ідентифікації';
+$lang['web_access_control_list'] = 'Список регулювання доступу';
+$lang['web_access_control_name'] = 'ім&#39;я';
+$lang['web_access_control_outside'] = 'зовні';
+$lang['web_access_control_outside_range'] = 'Зовнішні обмеження за часом';
+$lang['web_access_control_priority'] = 'пріоритет';
+$lang['web_access_control_start_time'] = 'Час початку';
+$lang['web_access_control_time_definitions'] = 'Час доби Визначення';
+$lang['web_access_control_time_delete_warning'] = 'Увага: будь-які елементи управління доступом з використанням цього визначення часу будуть видалені.';
+$lang['web_access_control_time_of_day'] = 'Час дня';
+$lang['web_access_control_time_restriction'] = 'обмеження';
+$lang['web_access_control_time_rule'] = 'Час-Day контролю доступу';
+$lang['web_access_control_type'] = 'тип';
+$lang['web_access_control_web_access_control'] = 'Контроль Web Access';
+$lang['web_access_control_within'] = 'в';
+$lang['web_access_control_within_range'] = 'У тимчасових обмежень';
