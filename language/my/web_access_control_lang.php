9f8e7d6f5cfd217259bf11937d776ec15bdf1d57
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:15:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'အချိန်ကာလ Add';
+$lang['web_access_control_app_description'] = 'Web ကို Access Control ကို web proxy ကို အသုံးပြု. အုပ်စုများသို့မဟုတ်ကွန်ပျူတာများဖို့အချိန်-of-နေ့ကကို web access ကို (အိုင်ပီသို့မဟုတ် MAC address ကို) ပြဋ္ဌာန်းရန်တစ်ခုအုပ်ချုပ်ရေးမှူးခွင့်ပြုပါတယ်။';
+$lang['web_access_control_app_name'] = 'Web ကို Access Control';
+$lang['web_access_control_apply_group'] = 'Group ကိုမှ Access Control Apply';
+$lang['web_access_control_apply_ip'] = 'IP Address ကိုမှ Access Control Apply';
+$lang['web_access_control_apply_mac'] = 'ရဲ့ MAC Address ကိုမှ Access Control Apply';
+$lang['web_access_control_ctrl_click'] = '(မျိုးစုံကိုရွေးချယ်ဘို့ CTRL-click)';
+$lang['web_access_control_day_of_week'] = 'အပတ်၏နေ့';
+$lang['web_access_control_end_time'] = 'အဆုံးအချိန်';
+$lang['web_access_control_id_method'] = 'သက်သေခံ၏ Method ကို';
+$lang['web_access_control_list'] = 'Access Control List ကို';
+$lang['web_access_control_name'] = 'Name ကို';
+$lang['web_access_control_outside'] = 'အပြင်မှာ';
+$lang['web_access_control_outside_range'] = 'အချိန်ကန့်သတ်အပြင်ဘက်တွင်';
+$lang['web_access_control_priority'] = 'ဦးစားပေး';
+$lang['web_access_control_start_time'] = 'Start ကိုအချိန်';
+$lang['web_access_control_time_definitions'] = 'နေ့ဆိုချက်၏အချိန်';
+$lang['web_access_control_time_delete_warning'] = 'သတိပေးချက်: ဤအချိန်နှင့်အဓိပ္ပါယ် အသုံးပြု. မဆို access ကိုထိန်းချုပ် entries တွေကိုဖျက်ပစ်ပါလိမ့်မည်။';
+$lang['web_access_control_time_of_day'] = 'နေ့၏အချိန်';
+$lang['web_access_control_time_restriction'] = 'ကန့်ကွက်ခြင်း';
+$lang['web_access_control_time_rule'] = 'အချိန်-of-Day ကို Access Control';
+$lang['web_access_control_type'] = 'ပုံစံ';
+$lang['web_access_control_web_access_control'] = 'Web ကို Access Control';
+$lang['web_access_control_within'] = 'အတွင်း';
+$lang['web_access_control_within_range'] = 'အချိန်ကန့်သတ်အတွင်းမှာပဲ';
