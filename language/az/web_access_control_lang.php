dc73b85ec427a6a8293deaab7579b46c48eb3a2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:15:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Müddət əlavə et';
+$lang['web_access_control_app_description'] = 'Web Access Control qruplar və ya web proxy istifadə kompüter (IP və ya MAC ünvanı) üçün vaxt-of-gün web girmə imkanı həyata bir administrator imkan verir.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Group Access Control müraciət';
+$lang['web_access_control_apply_ip'] = 'IP Ünvan Access Control müraciət';
+$lang['web_access_control_apply_mac'] = 'MAC Ünvan Access Control müraciət';
+$lang['web_access_control_ctrl_click'] = '(Birdən çox Marka üçün CTRL-basın)';
+$lang['web_access_control_day_of_week'] = 'Həftənin günü';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Müəyyənləşdirilməsi metodu';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'ad';
+$lang['web_access_control_outside'] = 'xaricində';
+$lang['web_access_control_outside_range'] = 'Outside vaxt məhdudiyyətləri';
+$lang['web_access_control_priority'] = 'Prioritet';
+$lang['web_access_control_start_time'] = 'Start Time';
+$lang['web_access_control_time_definitions'] = 'Day anlayışlar Time';
+$lang['web_access_control_time_delete_warning'] = 'Warning: bu dəfə müəyyən istifadə edərək hər hansı kontrol entries silinəcək.';
+$lang['web_access_control_time_of_day'] = 'Günü Time';
+$lang['web_access_control_time_restriction'] = 'məhdudiyyət';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'Type';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'ərzində';
+$lang['web_access_control_within_range'] = 'vaxt məhdudiyyətləri çərçivəsində';
