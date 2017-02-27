9dbc1df3f0e0ccfeb3b332d85dcf7e5e3560fe44
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:26:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Engeza Ubude besikhathi';
+$lang['web_access_control_app_description'] = 'Web Access Control ivumela umlawuli ukuqiniseka ukuthi uyagcinwa lowo isikhathi-of-day ukufinyelela web emaqenjini noma amakhompyutha (IP noma ikheli MAC) usebenzisa i-proxy iwebhu.';
+$lang['web_access_control_app_name'] = 'Access Control Web';
+$lang['web_access_control_apply_group'] = 'Zisebenza Access Control Group';
+$lang['web_access_control_apply_ip'] = 'Zisebenza Access Control Ikheli le-IP';
+$lang['web_access_control_apply_mac'] = 'Zisebenza Access Control ku Ikheli le-MAC';
+$lang['web_access_control_ctrl_click'] = '(CTRL chofoza-for okukhethile multiple)';
+$lang['web_access_control_day_of_week'] = 'Usuku Iviki';
+$lang['web_access_control_end_time'] = 'End Time';
+$lang['web_access_control_id_method'] = 'Indlela Identification';
+$lang['web_access_control_list'] = 'Access Control Uhlu';
+$lang['web_access_control_name'] = 'Igama';
+$lang['web_access_control_outside'] = 'Ngaphandle';
+$lang['web_access_control_outside_range'] = 'Ngaphandle imikhawulo isikhathi';
+$lang['web_access_control_priority'] = 'Priority';
+$lang['web_access_control_start_time'] = 'Qala Time';
+$lang['web_access_control_time_definitions'] = 'Isikhathi Day Izincazelo';
+$lang['web_access_control_time_delete_warning'] = 'Isexwayiso: noma yikuphi control ukufinyelela okufakiwe usebenzisa lesi sikhathi ncazelo kuzosuswa.';
+$lang['web_access_control_time_of_day'] = 'Isikhathi Day';
+$lang['web_access_control_time_restriction'] = 'Okuvumelekile';
+$lang['web_access_control_time_rule'] = 'Isikhathi-of-Day Access Control';
+$lang['web_access_control_type'] = 'Uhlobo';
+$lang['web_access_control_web_access_control'] = 'Access Control Web';
+$lang['web_access_control_within'] = 'Kungakapheli';
+$lang['web_access_control_within_range'] = 'Kungakapheli imikhawulo isikhathi';
