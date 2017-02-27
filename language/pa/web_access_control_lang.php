0b5ea91c4d6c273051d3dd3692b8a620dfbf88a4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:22:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'ਵਾਰ ਦੇ ਅਰਸੇ ਵਿੱਚ ਜੋੜੋ';
+$lang['web_access_control_app_description'] = 'ਵੈੱਬ ਪਹੁੰਚ ਕੰਟਰੋਲ ਗਰੁੱਪ ਜ ਕੰਪਿਊਟਰ (IP ਨੂੰ ਜ MAC ਐਡਰੈੱਸ) ਵੈੱਬ ਪਰਾਕਸੀ ਵਰਤ ਕਰਨ ਲਈ ਵਾਰ-ਦੇ-ਦਿਨ ਵੈੱਬ ਪਹੁੰਚ ਨੂੰ ਲਾਗੂ ਕਰਨ ਲਈ ਇੱਕ ਪਰਬੰਧਕ ਲਈ ਸਹਾਇਕ ਹੈ.';
+$lang['web_access_control_app_name'] = 'ਵੈੱਬ ਪਹੁੰਚ ਕੰਟਰੋਲ';
+$lang['web_access_control_apply_group'] = 'ਗਰੁੱਪ ਤੱਕ ਪਹੁੰਚ ਕੰਟਰੋਲ ਲਾਗੂ';
+$lang['web_access_control_apply_ip'] = 'IP ਪਤਾ ਕਰਨ ਲਈ ਪਹੁੰਚ ਕੰਟਰੋਲ ਲਾਗੂ';
+$lang['web_access_control_apply_mac'] = 'MAC ਐਡਰੈੱਸ ਤੱਕ ਪਹੁੰਚ ਕੰਟਰੋਲ ਲਾਗੂ';
+$lang['web_access_control_ctrl_click'] = '(ਮਲਟੀਪਲ ਚੋਣ ਲਈ Ctrl-ਕਲਿੱਕ ਕਰੋ)';
+$lang['web_access_control_day_of_week'] = 'ਹਫਤੇ ਦੇ ਦਿਨ';
+$lang['web_access_control_end_time'] = 'ਅੰਤ ਟਾਈਮ';
+$lang['web_access_control_id_method'] = 'ਪਛਾਣ ਦੇ ਢੰਗ';
+$lang['web_access_control_list'] = 'ਪਹੁੰਚ ਕੰਟਰੋਲ ਸੂਚੀ';
+$lang['web_access_control_name'] = 'ਦਾ ਨਾਮ';
+$lang['web_access_control_outside'] = 'ਬਾਹਰ';
+$lang['web_access_control_outside_range'] = 'ਬਾਹਰ ਵਾਰ ਪਾਬੰਦੀ';
+$lang['web_access_control_priority'] = 'ਤਰਜੀਹ';
+$lang['web_access_control_start_time'] = 'ਸਟਾਰਟ ਟਾਈਮ';
+$lang['web_access_control_time_definitions'] = 'ਦਿਵਸ ਦੇ ਅਰਥ ਦਾ ਟਾਈਮ';
+$lang['web_access_control_time_delete_warning'] = 'ਚੇਤਾਵਨੀ: ਕੋਈ ਵੀ ਪਹੁੰਚ ਕੰਟਰੋਲ ਨੂੰ ਇਸ ਵਾਰ ਪਰਿਭਾਸ਼ਾ ਵਰਤ ਇੰਦਰਾਜ਼ ਨੂੰ ਹਟਾ ਦਿੱਤਾ ਜਾਵੇਗਾ.';
+$lang['web_access_control_time_of_day'] = 'ਦਿਵਸ ਦੇ ਟਾਈਮ';
+$lang['web_access_control_time_restriction'] = 'ਪਾਬੰਦੀ';
+$lang['web_access_control_time_rule'] = 'ਟਾਈਮ-ਦੇ-ਦਿਵਸ ਪਹੁੰਚ ਕੰਟਰੋਲ';
+$lang['web_access_control_type'] = 'ਦੀ ਕਿਸਮ';
+$lang['web_access_control_web_access_control'] = 'ਵੈੱਬ ਪਹੁੰਚ ਕੰਟਰੋਲ';
+$lang['web_access_control_within'] = 'ਵਿਖੇ';
+$lang['web_access_control_within_range'] = 'ਵਾਰ ਪਾਬੰਦੀ ਦੇ ਅੰਦਰ';
