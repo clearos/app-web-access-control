db446ffcffa4123872a2dbc39015b36841228ae3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:14:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Voeg Tyd Tydperk';
+$lang['web_access_control_app_description'] = 'Web Access Control laat &#39;n administrateur om tyd-van-dag web toegang tot groepe of rekenaars (IP of MAC-adres) met behulp van die web proxy af te dwing.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Pas toegangsbeheer tot Groep';
+$lang['web_access_control_apply_ip'] = 'Pas toegangsbeheer tot IP-adres';
+$lang['web_access_control_apply_mac'] = 'Pas toegangsbeheer tot MAC-adres';
+$lang['web_access_control_ctrl_click'] = '(Ctrl-klik vir veelvuldige keuse)';
+$lang['web_access_control_day_of_week'] = 'Dag van die week';
+$lang['web_access_control_end_time'] = 'eindtyd';
+$lang['web_access_control_id_method'] = 'Metode van identifikasie';
+$lang['web_access_control_list'] = 'Toegangsbeheer Lys';
+$lang['web_access_control_name'] = 'naam';
+$lang['web_access_control_outside'] = 'buite';
+$lang['web_access_control_outside_range'] = 'Buite beperkings tyd';
+$lang['web_access_control_priority'] = 'prioriteit';
+$lang['web_access_control_start_time'] = 'Begin Tyd';
+$lang['web_access_control_time_definitions'] = 'Tyd van die dag Definisies';
+$lang['web_access_control_time_delete_warning'] = 'Waarskuwing: enige toegangsbeheer inskrywings met behulp van hierdie tyd definisie sal uit gewis word.';
+$lang['web_access_control_time_of_day'] = 'Tyd van die dag';
+$lang['web_access_control_time_restriction'] = 'beperking';
+$lang['web_access_control_time_rule'] = 'Tyd van die dag Toegangsbeheer';
+$lang['web_access_control_type'] = 'tipe';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'binne';
+$lang['web_access_control_within_range'] = 'Binne tyd beperkinge';
