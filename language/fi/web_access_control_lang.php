af3dc5817617caa8cb2eb44bf78b283c3800d12c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:17:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Lisää aikajakso';
+$lang['web_access_control_app_description'] = 'Web Access Control avulla pääkäyttäjä voi valvoa aikaa ja päivän web pääsy ryhmiin tai tietokoneille (IP tai MAC-osoite) käyttämällä web proxy.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Käytä Access Control Group';
+$lang['web_access_control_apply_ip'] = 'Käytä Access Control IP-osoite';
+$lang['web_access_control_apply_mac'] = 'Käytä Access Control MAC-osoite';
+$lang['web_access_control_ctrl_click'] = '(CTRL-klikkaa useita valintoja)';
+$lang['web_access_control_day_of_week'] = 'Viikonpäivä';
+$lang['web_access_control_end_time'] = 'Loppu aika';
+$lang['web_access_control_id_method'] = 'Menetelmä Tunnistus';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'Nimi';
+$lang['web_access_control_outside'] = 'Ulkopuolella';
+$lang['web_access_control_outside_range'] = 'Ulkopuolella aikarajoituksia';
+$lang['web_access_control_priority'] = 'prioriteetti';
+$lang['web_access_control_start_time'] = 'Aloitusaika';
+$lang['web_access_control_time_definitions'] = 'Kellonaika Määritelmät';
+$lang['web_access_control_time_delete_warning'] = 'Varoitus: kaikki kulunvalvonta merkinnät tällä kertaa määritelmä poistetaan.';
+$lang['web_access_control_time_of_day'] = 'Vuorokaudenaika';
+$lang['web_access_control_time_restriction'] = 'rajoitus';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'Tyyppi';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Sisällä';
+$lang['web_access_control_within_range'] = 'Within aikarajoituksia';
