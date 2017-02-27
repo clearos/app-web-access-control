875cdcab196d8771572a95243947bbecf0ce6ef8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:21:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Żid Ħin Perjodu';
+$lang['web_access_control_app_description'] = 'Web Access Control tippermetti amministratur biex tinforza web aċċess f&#39;ħin tal-jum għal gruppi jew kompjuters (IP jew jindirizzaw MAC) li jużaw l-prokura web.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Applika Kontroll ta &#39;Aċċess għall-Grupp';
+$lang['web_access_control_apply_ip'] = 'Applika Kontroll ta &#39;Aċċess għall Indirizz IP';
+$lang['web_access_control_apply_mac'] = 'Applika Kontroll ta &#39;Aċċess għall MAC Indirizz';
+$lang['web_access_control_ctrl_click'] = '(CTRL click għall selezzjonijiet multipli)';
+$lang['web_access_control_day_of_week'] = 'Jum tal-Ġimgħa';
+$lang['web_access_control_end_time'] = 'Ħin tmiem';
+$lang['web_access_control_id_method'] = 'Metodu ta &#39;Identifikazzjoni';
+$lang['web_access_control_list'] = 'Aċċess Lista ta &#39;Kontroll';
+$lang['web_access_control_name'] = 'isem';
+$lang['web_access_control_outside'] = 'barra';
+$lang['web_access_control_outside_range'] = 'restrizzjonijiet ta &#39;żmien barra';
+$lang['web_access_control_priority'] = 'prijorità';
+$lang['web_access_control_start_time'] = 'bidu Time';
+$lang['web_access_control_time_definitions'] = 'Ħin tal-ġurnata Definizzjonijiet';
+$lang['web_access_control_time_delete_warning'] = 'Twissija: Id-dħul kollha kontroll ta &#39;aċċess li jużaw din id-definizzjoni darba se jitħassru.';
+$lang['web_access_control_time_of_day'] = 'Ħin tal-Jum';
+$lang['web_access_control_time_restriction'] = 'restrizzjoni';
+$lang['web_access_control_time_rule'] = 'Ħin ta-Jum Kontroll ta &#39;Aċċess';
+$lang['web_access_control_type'] = 'tip';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Fi ħdan';
+$lang['web_access_control_within_range'] = 'Fi ħdan restrizzjonijiet ta &#39;żmien';
