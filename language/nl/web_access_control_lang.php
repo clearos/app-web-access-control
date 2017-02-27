27a689bf8dfc6564f1afd587b37ee855a81a020b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:21:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Voeg Periode';
+$lang['web_access_control_app_description'] = 'Web Access Control kan een beheerder om de tijd van de dag toegang tot het web aan groepen of computers (IP- of MAC-adres) met behulp van de web proxy af te dwingen.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Solliciteer Access Control Group';
+$lang['web_access_control_apply_ip'] = 'Solliciteer Access Control om IP-adres';
+$lang['web_access_control_apply_mac'] = 'Solliciteer Access Control om MAC-adres';
+$lang['web_access_control_ctrl_click'] = '(CTRL-klik voor meerdere selecties)';
+$lang['web_access_control_day_of_week'] = 'Dag van de week';
+$lang['web_access_control_end_time'] = 'Eindtijd';
+$lang['web_access_control_id_method'] = 'Wijze van identificatie';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'Naam';
+$lang['web_access_control_outside'] = 'Buiten';
+$lang['web_access_control_outside_range'] = 'Buiten beperkingen tijd';
+$lang['web_access_control_priority'] = 'Prioriteit';
+$lang['web_access_control_start_time'] = 'Starttijd';
+$lang['web_access_control_time_definitions'] = 'Tijd van de dag definities';
+$lang['web_access_control_time_delete_warning'] = 'Waarschuwing: elke access control data met behulp van deze tijd definitie zullen worden verwijderd.';
+$lang['web_access_control_time_of_day'] = 'Tijdstip';
+$lang['web_access_control_time_restriction'] = 'Beperking';
+$lang['web_access_control_time_rule'] = 'Time-of-Day Access Control';
+$lang['web_access_control_type'] = 'Type';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Binnen';
+$lang['web_access_control_within_range'] = 'Binnen de tijd beperkingen';
