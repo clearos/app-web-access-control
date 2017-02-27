2564dd204a7d8e375e0edbb6821211be00ab06a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 10:20:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/web_access_control_lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['web_access_control_add_time'] = 'Add Time-potoana';
+$lang['web_access_control_app_description'] = 'Web Access Control mamela ny mpitantana ny mampihatra fotoana-ny-andro tranonkala ny fidirana amin&#39;ny vondrona na ny solosaina (IP na adiresy MAC) mampiasa ny tranonkala hisolo.';
+$lang['web_access_control_app_name'] = 'Web Access Control';
+$lang['web_access_control_apply_group'] = 'Ampiharo Access Control ho Group';
+$lang['web_access_control_apply_ip'] = 'Ampiharo Access Control ny IP Address';
+$lang['web_access_control_apply_mac'] = 'Ampiharo Access Control ho MAC Address';
+$lang['web_access_control_ctrl_click'] = '(Ctrl-Tsindrio ny maro voafantina)';
+$lang['web_access_control_day_of_week'] = 'Andron&#39;ny Week';
+$lang['web_access_control_end_time'] = 'Ny Time';
+$lang['web_access_control_id_method'] = 'Fomba Famantarana';
+$lang['web_access_control_list'] = 'Access Control List';
+$lang['web_access_control_name'] = 'Name';
+$lang['web_access_control_outside'] = 'Outside';
+$lang['web_access_control_outside_range'] = 'Outside ny famerana';
+$lang['web_access_control_priority'] = 'laharam-pahamehana';
+$lang['web_access_control_start_time'] = 'Start Time';
+$lang['web_access_control_time_definitions'] = 'Time ny Day Famaritana';
+$lang['web_access_control_time_delete_warning'] = 'Fampitandremana: misy fanaraha-maso ny fidirana anglisy mampiasa io fotoana io famaritana ho voafafa.';
+$lang['web_access_control_time_of_day'] = 'Time ny Day';
+$lang['web_access_control_time_restriction'] = 'fameperana';
+$lang['web_access_control_time_rule'] = 'Time-ny-Day Access Control';
+$lang['web_access_control_type'] = 'Type';
+$lang['web_access_control_web_access_control'] = 'Web Access Control';
+$lang['web_access_control_within'] = 'Ao anatin&#39;ny';
+$lang['web_access_control_within_range'] = 'Ao anatin&#39;ny fotoana famerana';
